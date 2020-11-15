34190d8ad8a2c4b7a04038af820ea9efa5d2ef1c
heff <git@heff.me>
2020-11-14 17:11:19 -0800

    Added 'rates' attribute

--- src/js/media-playback-rate-button.js
+++ src/js/media-playback-rate-button.js
@@ -1,14 +1,37 @@
 import MediaChromeButton from './media-chrome-button.js';
 import { defineCustomElement } from './utils/defineCustomElement.js';
 
+/*
+  <media-playback-rate-button rates="1,1.5,2">
+*/
+
+const DEFAULT_RATES = [1, 1.25, 1.5, 1.75, 2];
+
 class MediaPlaybackRateButton extends MediaChromeButton {
   constructor() {
     super();
-    this._rates = [1,1.25,1.5,1.75,2];
+    this._rates = DEFAULT_RATES;
   }
 
   static get observedAttributes() {
     return ['rates'].concat(super.observedAttributes || []);
+  }
+
+  // String of comma separated values, to match attribute
+  set rates(rates) {
+    if (!rates) {
+      this._rates = DEFAULT_RATES;
+    } else {
+      if (typeof rates == 'string') {
+        rates = rates.split(/,\s?/)
+      }
+
+      this._rates = rates;
+    }
+  }
+
+  get rates() {
+    return this._rates;
   }
 
   onClick(e) {
@@ -17,9 +40,9 @@ class MediaPlaybackRateButton extends MediaChromeButton {
     if (!media) return;
 
     const currentRate = media.playbackRate;
-    let newRate = this._rates.find(r => r > currentRate);
+    let newRate = this.rates.find(r => r > currentRate);
 
-    if (!newRate) newRate = this._rates[0];
+    if (!newRate) newRate = this.rates[0];
 
     media.playbackRate = newRate;
   }
