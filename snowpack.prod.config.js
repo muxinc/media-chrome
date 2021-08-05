6b916dbd788af02836bf0dbf436f3f06c75208cc
Christian Pillsbury <cjpillsbury@gmail.com>
2021-08-04 21:05:01 -0500

    Improv/dev env snowpack (#45)
    * chore/cleanup-package-lock: update package-lock based on audit fails.
    
    * improv/dev-env: migrate from npm (package-lock) to yarn (yarn.lock).
    
    * improv/dev-env: Update npm scripts to use yarn instead of npm
    
    * improv/dev-env-snowpack: migrate build infrastructure to snowpack. Update package.json and yarn.lock based on dep changes and scripts. Migrate single ui element examples to directly reference their element source.
    
    * improv/dev-env-snowpack: fix pathing from ToC example page to use relative paths.
    
    * improv/dev-env-snowpack: Add doctype to example html.
    
    * improv/dev-env-snowpack: Add vercel config for better experience on vercel demo app.
    
    * improv/dev-env-snowpack: minor version bump.

---
+++ snowpack.prod.config.js
@@ -0,0 +1,27 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  extends: './snowpack.common.config.js',
+  mount: {
+    'src/js': { url: '/' },
+  },
+  optimize: {
+    bundle: false,
+    minify: true,
+    target: 'es2019',
+  },
+  plugins: [
+    /* ... */
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    out: './dist'
+  },
+};
