2d86620344c234be7f518e3f0f33ce7f19bfef14
Christian Pillsbury <cjpillsbury@gmail.com>
2021-08-16 12:03:09 -0500

    feat(mux-video-init): Adding basic integration of mux-embed (untested).

---
+++ packages/mux-video/src/customTypings/mux-embed.d.ts
@@ -0,0 +1,5 @@
+/** @TODO Add type defs to mux-embed directly */
+declare module 'mux-embed' {
+    /** @TODO Add better type def for options */
+    export function monitor(id: string | HTMLMediaElement, options: {}) {}
+}
--- packages/mux-video/src/index.ts
+++ packages/mux-video/src/index.ts
@@ -1,14 +1,32 @@
 // Web Components: Extending Native Elements, A working example
 
-import CustomVideoElement from 'custom-video-element';
-import Hls from 'hls.js';
+import CustomVideoElement from "custom-video-element";
+import mux from "mux-embed";
+
+import Hls from "hls.js";
+
+type Attributes = {
+  ENV_KEY: "env-key";
+};
+
+const Attributes: Attributes = {
+  ENV_KEY: "env-key",
+};
 
 class MuxVideoElement extends CustomVideoElement {
+  static get observedAttributes() {
+    return [
+      Attributes.ENV_KEY,
+      ...(CustomVideoElement.observedAttributes ?? []),
+    ];
+  }
 
   protected __hls?: Hls;
+  protected __muxPlayerInitTime: number;
 
   constructor() {
     super();
+    this.__muxPlayerInitTime = Date.now();
   }
 
   get hls() {
@@ -19,27 +37,29 @@ class MuxVideoElement extends CustomVideoElement {
     // Use the attribute value as the source of truth.
     // No need to store it in two places.
     // This avoids needing a to read the attribute initially and update the src.
-    return this.getAttribute('src');
+    return this.getAttribute("src");
   }
 
   set src(val) {
     // If being set by attributeChangedCallback,
     // dont' cause an infinite loop
     if (val === this.src) return;
-    
+
     if (val == null) {
-      this.removeAttribute('src');
+      this.removeAttribute("src");
     } else {
-      this.setAttribute('src', val);
+      this.setAttribute("src", val);
     }
   }
 
   load() {
     /** @TODO Add custom errors + error codes */
     if (!this.src) {
-      console.error('DONT DO THIS');
+      console.error("DONT DO THIS");
       return;
     }
+
+    const env_key = this.getAttribute(Attributes.ENV_KEY);
 
     if (Hls.isSupported()) {
       const hls = new Hls({
@@ -52,8 +72,36 @@ class MuxVideoElement extends CustomVideoElement {
 
       this.__hls = hls;
 
-    } else if (this.nativeEl.canPlayType('application/vnd.apple.mpegurl')) {
+      if (env_key) {
+        mux.monitor(this.nativeEl, {
+          debug: false,
+          hlsjs: hls,
+          Hls: Hls,
+          data: {
+            env_key, // required
+            // Metadata fields
+            player_name: "mux-video", // any arbitrary string you want to use to identify this player
+            // Should this be the initialization of *THIS* player (instance) or the page?
+            player_init_time: this.__muxPlayerInitTime, // ex: 1451606400000
+            // ...
+          },
+        });
+      }
+    } else if (this.nativeEl.canPlayType("application/vnd.apple.mpegurl")) {
       this.nativeEl.src = this.src;
+      if (env_key) {
+        mux.monitor(this.nativeEl, {
+          debug: false,
+          data: {
+            env_key, // required
+            // Metadata fields
+            player_name: "mux-video", // any arbitrary string you want to use to identify this player
+            // Should this be the initialization of *THIS* player (instance) or the page?
+            player_init_time: this.__muxPlayerInitTime, // ex: 1451606400000
+            // ...
+          },
+        });
+      }
     }
   }
 
@@ -81,12 +129,14 @@ class MuxVideoElement extends CustomVideoElement {
 }
 
 declare global {
-  interface Window { MuxVideoElement: typeof MuxVideoElement; }
+  interface Window {
+    MuxVideoElement: typeof MuxVideoElement;
+  }
 }
 
 /** @TODO Refactor once using `globalThis` polyfills */
-if (!window.customElements.get('mux-video')) {
-  window.customElements.define('mux-video', MuxVideoElement);
+if (!window.customElements.get("mux-video")) {
+  window.customElements.define("mux-video", MuxVideoElement);
   /** @TODO consider externalizing this (breaks standard modularity) */
   window.MuxVideoElement = MuxVideoElement;
 }
