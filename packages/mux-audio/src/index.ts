d5229d26be74338158a4037ff82f23123011cc6c
Christian Pillsbury <cjpillsbury@gmail.com>
2021-08-20 14:31:51 -0500

    feat: support hls audio as first pass impl. Re-add playback-id support.

--- packages/mux-audio/src/index.ts
+++ packages/mux-audio/src/index.ts
@@ -2,12 +2,15 @@ import "./polyfills/window";
 import CustomAudioElement from "./CustomAudioElement";
 import mux, { Options, HighPriorityMetadata } from "mux-embed";
 
+import Hls from "hls.js";
+
 type Metadata = Partial<Options["data"]>;
 
 /** @TODO make the relationship between name+value smarter and more deriveable (CJP) */
 type AttributeNames = {
   ENV_KEY: "env-key";
   DEBUG: "debug";
+  PLAYBACK_ID: "playback-id";
   METADATA_URL: "metadata-url";
   PREFER_NATIVE: "prefer-native";
   METADATA_VIDEO_ID: "metadata-video-id";
@@ -18,6 +21,7 @@ type AttributeNames = {
 const Attributes: AttributeNames = {
   ENV_KEY: "env-key",
   DEBUG: "debug",
+  PLAYBACK_ID: "playback-id",
   METADATA_URL: "metadata-url",
   PREFER_NATIVE: "prefer-native",
   METADATA_VIDEO_ID: "metadata-video-id",
@@ -27,7 +31,38 @@ const Attributes: AttributeNames = {
 
 const AttributeNameValues = Object.values(Attributes);
 
-type HTMLAudioElementWithMux = HTMLAudioElement & { mux?: typeof mux };
+const toPlaybackIdParts = (
+  playbackIdWithOptionalParams: string
+): [string, string?] => {
+  const qIndex = playbackIdWithOptionalParams.indexOf("?");
+  const idPart = playbackIdWithOptionalParams.slice(0, qIndex);
+  const queryPart = playbackIdWithOptionalParams.slice(qIndex);
+  if (!queryPart) return [idPart];
+  return [idPart, queryPart];
+};
+
+const toMuxVideoURL = (playbackId: string | null) => {
+  if (!playbackId) return null;
+  const [idPart, queryPart = ""] = toPlaybackIdParts(playbackId);
+  return `https://stream.mux.com/${idPart}.m3u8${queryPart}`;
+};
+
+const hlsSupported = Hls.isSupported();
+
+type HTMLAudioElementWithMux = HTMLVideoElement & { mux?: typeof mux };
+
+const getPlaybackIdAsVideoIdMetadata = (
+  mediaEl: MuxAudioElement
+): Partial<Pick<HighPriorityMetadata, "video_id">> => {
+  const playbackIdWithOptionalParams = mediaEl.getAttribute(
+    Attributes.PLAYBACK_ID
+  );
+  if (!playbackIdWithOptionalParams) return {};
+  const [playbackId] = toPlaybackIdParts(playbackIdWithOptionalParams);
+  if (!playbackId) return {};
+
+  return { video_id: playbackId };
+};
 
 const getHighPriorityMetadata = (
   mediaEl: MuxAudioElement
@@ -53,12 +88,17 @@ class MuxAudioElement extends CustomAudioElement<HTMLAudioElementWithMux> {
     ];
   }
 
+  protected __hls?: Hls;
   protected __muxPlayerInitTime: number;
   protected __metadata: Readonly<Metadata> = {};
 
   constructor() {
     super();
     this.__muxPlayerInitTime = Date.now();
+  }
+
+  get hls() {
+    return this.__hls;
   }
 
   get mux(): Readonly<typeof mux> | undefined {
@@ -141,11 +181,31 @@ class MuxAudioElement extends CustomAudioElement<HTMLAudioElementWithMux> {
 
     const env_key = this.getAttribute(Attributes.ENV_KEY);
     const debug = this.debug;
+    const preferNative = this.preferNative;
+    const canUseNative = this.nativeEl.canPlayType(
+      "application/vnd.apple.mpegurl"
+    );
+
+    // We should use native playback if we a) can use native playback and either b) prefer it or c) can't use Hls.js
+    const shouldUseNative = canUseNative && (preferNative || !hlsSupported);
+
+    // 1. create hls if we should be using it "under the hood"
+    if (!shouldUseNative && hlsSupported) {
+      const hls = new Hls({
+        // Kind of like preload metadata, but causes spinner.
+        // autoStartLoad: false,
+        debug,
+      });
+
+      this.__hls = hls;
+    }
 
     // 2. Start monitoring for mux data before we do anything else
     if (env_key) {
       const player_init_time = this.__muxPlayerInitTime;
       const metadataObj = this.__metadata;
+      const hlsjs = this.__hls; // an instance of hls.js or undefined
+      const playbackIdMetadata = getPlaybackIdAsVideoIdMetadata(this);
       const highPriorityMetadata = getHighPriorityMetadata(this);
       /**
        * @TODO Use documented version if/when resolved (commented out below) (CJP)
@@ -159,12 +219,16 @@ class MuxAudioElement extends CustomAudioElement<HTMLAudioElementWithMux> {
 
       mux.monitor(this.nativeEl, {
         debug,
+        hlsjs,
+        Hls: hlsjs ? Hls : undefined,
         data: {
           env_key, // required
           // Metadata fields
           player_name: "mux-audio", // default player name for "mux-audio"
           player_version,
           player_init_time,
+          // Default to playback-id as video_id (if available)
+          ...playbackIdMetadata,
           // Use any metadata passed in programmatically (which may override the defaults above)
           ...metadataObj,
           // Use any high priority metadata passed in via attributes (which may override any of the above)
@@ -173,11 +237,52 @@ class MuxAudioElement extends CustomAudioElement<HTMLAudioElementWithMux> {
       });
     }
 
-    /** @TODO Make this a little beefier for formats that aren't supported (CJP) */
-    this.nativeEl.src = this.src;
+    // 3. Finish any additional setup to load/play the media
+    if (shouldUseNative) {
+      this.nativeEl.src = this.src;
+    } else if (this.__hls) {
+      const hls = this.__hls;
+
+      hls.on(Hls.Events.ERROR, (_event, data) => {
+        if (data.fatal) {
+          switch (data.type) {
+            case Hls.ErrorTypes.NETWORK_ERROR:
+              // try to recover network error
+              console.error("fatal network error encountered, try to recover");
+              hls.startLoad();
+              break;
+            case Hls.ErrorTypes.MEDIA_ERROR:
+              console.error("fatal media error encountered, try to recover");
+              hls.recoverMediaError();
+              break;
+            default:
+              // cannot recover
+              console.error(
+                "unrecoverable fatal error encountered, cannot recover (check logs for more info)"
+              );
+              hls.destroy();
+              break;
+          }
+        }
+      });
+
+      hls.loadSource(this.src);
+      hls.attachMedia(this.nativeEl);
+    } else {
+      console.error(
+        "It looks like HLS video playback will not work on this system! If possible, try upgrading to the newest versions of your browser or software."
+      );
+      return;
+    }
   }
 
   unload() {
+    // NOTE: I believe we cannot reliably "recycle" hls player instances, but should confirm at least for optimization reasons.
+    if (this.__hls) {
+      this.__hls.detachMedia();
+      this.__hls.destroy();
+      this.__hls = undefined;
+    }
     if (this.nativeEl.mux) {
       this.nativeEl.mux.destroy();
       delete this.nativeEl.mux;
@@ -203,6 +308,10 @@ class MuxAudioElement extends CustomAudioElement<HTMLAudioElementWithMux> {
           this.load();
         }
         break;
+      case Attributes.PLAYBACK_ID:
+        /** @TODO Improv+Discuss - how should playback-id update wrt src attr changes (and vice versa) (CJP) */
+        this.src = toMuxVideoURL(newValue);
+        break;
       case Attributes.DEBUG:
         const debug = this.debug;
         if (!!this.mux) {
@@ -210,6 +319,9 @@ class MuxAudioElement extends CustomAudioElement<HTMLAudioElementWithMux> {
           console.info(
             "Cannot toggle debug mode of mux data after initialization. Make sure you set all metadata to override before setting the src."
           );
+        }
+        if (!!this.hls) {
+          this.hls.config.debug = debug;
         }
         break;
       case Attributes.METADATA_URL:
@@ -251,5 +363,7 @@ if (!window.customElements.get("mux-audio")) {
   /** @TODO consider externalizing this (breaks standard modularity) */
   window.MuxAudioElement = MuxAudioElement;
 }
+
+export { Hls };
 
 export default MuxAudioElement;
