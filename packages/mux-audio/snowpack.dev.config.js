344a463c49fce0312beccc8d349964746143edb8
Christian Pillsbury <cjpillsbury@gmail.com>
2021-08-23 11:40:10 -0500

    build: Add smarter routes and default urls for examples in snowpack dev usage.

--- packages/mux-audio/snowpack.dev.config.js
+++ packages/mux-audio/snowpack.dev.config.js
@@ -6,9 +6,19 @@ module.exports = {
   extends: "./snowpack.common.config.js",
   mount: {
     src: { url: "/dist" },
-    // Mount "public" to the root URL path ("/*") and serve files with zero transformations:
     examples: { url: "/examples", static: true, resolve: false },
   },
+  routes: [
+    {
+      match: "routes",
+      src: "/",
+      dest: (_req, resp) => {
+        resp.statusCode = 302;
+        resp.setHeader("Location", "/examples/index.html");
+        return resp.end();
+      },
+    },
+  ],
   plugins: [
     /* ... */
   ],
@@ -16,7 +26,7 @@ module.exports = {
     /* ... */
   },
   devOptions: {
-    /* ... */
+    openUrl: "/examples/index.html",
   },
   buildOptions: {},
 };
