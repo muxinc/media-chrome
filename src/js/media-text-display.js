db50c05119d523e136589890d8c12b9dbf605509
Christian Pillsbury <cjpillsbury@gmail.com>
2021-08-30 13:49:05 -0500

    bugfix: migrate components to support outside assoc w/media-controller (displays).

--- src/js/media-text-display.js
+++ src/js/media-text-display.js
@@ -1,3 +1,4 @@
+import { MediaUIAttributes } from './constants.js';
 import { defineCustomElement } from './utils/defineCustomElement.js';
 import { Window as window, Document as document } from './utils/server-safe-globals.js';
 // Todo: Use data locals: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleTimeString
@@ -37,12 +38,46 @@ template.innerHTML = `
 `;
 
 class MediaTextDisplay extends window.HTMLElement {
+  
+  static get observedAttributes() {
+    return [MediaUIAttributes.MEDIA_CONTROLLER];
+  }
+  
   constructor() {
     super();
 
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
     this.container = this.shadowRoot.querySelector('#container');
+  }
+  
+  attributeChangedCallback(attrName, oldValue, newValue) {
+    if (attrName === MediaUIAttributes.MEDIA_CONTROLLER) {
+      if (oldValue) {
+        const mediaControllerEl = document.getElementById(oldValue);
+        mediaControllerEl?.unassociateDescendantsOf?.(this);
+      }
+      if (newValue) {
+        const mediaControllerEl = document.getElementById(newValue);
+        mediaControllerEl?.associateDescendantsOf?.(this);
+      }
+    }
+  }
+
+  connectedCallback() {
+    const mediaControllerId = this.getAttribute(MediaUIAttributes.MEDIA_CONTROLLER);
+    if (mediaControllerId) {
+      const mediaControllerEl = document.getElementById(mediaControllerId);
+      mediaControllerEl?.associateDescendantsOf?.(this);
+    }
+  }
+
+  disconnectedCallback() {
+    const mediaControllerSelector = this.getAttribute(MediaUIAttributes.MEDIA_CONTROLLER);
+    if (mediaControllerSelector) {
+      const mediaControllerEl = document.getElementById(mediaControllerId);
+      mediaControllerEl?.unassociateDescendantsOf?.(this);
+    }
   }
 }
 
