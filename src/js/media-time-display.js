753191ab850813004001d1b244a254fe03d74d33
Christian Pillsbury <cjpillsbury@gmail.com>
2021-09-08 11:22:51 -0500

    improv/a11y-phase-1: Attempt to model media-time-display as progressbar for a11y (currently does not announce dynamic changes)

--- src/js/media-time-display.js
+++ src/js/media-time-display.js
@@ -1,6 +1,6 @@
 import MediaTextDisplay from './media-text-display.js';
 import { defineCustomElement } from './utils/defineCustomElement.js';
-import { formatTime } from './utils/time.js';
+import { formatAsTimePhrase, formatTime } from './utils/time.js';
 import { MediaUIAttributes } from './constants.js';
 // Todo: Use data locals: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleTimeString
 
@@ -20,6 +20,31 @@ const formatTimesLabel = (el, { timesSep = DEFAULT_TIMES_SEP } = {}) => {
   return `${timeLabel}${timesSep}${formatTime(duration)}`;
 };
 
+const DEFAULT_MISSING_TIME_PHRASE = 'video not loaded, unknown time.';
+
+const updateAriaValueText = (el) => {
+  if (!(el.hasAttribute(MediaUIAttributes.MEDIA_CURRENT_TIME) && el.hasAttribute(MediaUIAttributes.MEDIA_DURATION))) {
+    el.setAttribute('aria-valuetext', DEFAULT_MISSING_TIME_PHRASE);
+    return;
+  }
+  const showRemaining = el.getAttribute('remaining') != null;
+  const showDuration = el.getAttribute('show-duration') != null;
+  const currentTime = +el.getAttribute(MediaUIAttributes.MEDIA_CURRENT_TIME);
+  const duration = +el.getAttribute(MediaUIAttributes.MEDIA_DURATION);
+
+  const currentTimePhrase = showRemaining 
+    ? formatAsTimePhrase(0 - (duration - currentTime)) 
+    : formatAsTimePhrase(currentTime);
+
+  if (!showDuration) {
+    el.setAttribute('aria-valuetext', currentTimePhrase);
+    return;
+  }
+  const totalTimePhrase = formatAsTimePhrase(duration);
+  const fullPhrase = `${currentTimePhrase} of ${totalTimePhrase}`;
+  el.setAttribute('aria-valuetext', fullPhrase);
+};
+
 class MediaTimeDisplay extends MediaTextDisplay {
 
   static get observedAttributes() {
@@ -27,6 +52,9 @@ class MediaTimeDisplay extends MediaTextDisplay {
   }
 
   connectedCallback() {
+    this.setAttribute('role', 'progressbar');
+    this.setAttribute('aria-live', 'polite');
+    this.setAttribute('tabindex', 0);
     this.setAttribute(MediaUIAttributes.MEDIA_CHROME_ATTRIBUTES, this.constructor.observedAttributes.join(' '));
     super.connectedCallback();
   }
@@ -34,6 +62,7 @@ class MediaTimeDisplay extends MediaTextDisplay {
   attributeChangedCallback(attrName, oldValue, newValue) {
     if ([MediaUIAttributes.MEDIA_CURRENT_TIME, MediaUIAttributes.MEDIA_DURATION, 'remaining', 'show-duration'].includes(attrName)) {
       const timesLabel = formatTimesLabel(this);
+      updateAriaValueText(this);
       this.container.innerHTML = timesLabel;
     }
     super.attributeChangedCallback(attrName, oldValue, newValue);
