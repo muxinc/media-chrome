b3a6aaedbbc45d5b8fb4cf8932537334480e39e3
Christian Pillsbury <cjpillsbury@gmail.com>
2021-10-20 10:00:20 -0500

    chore/testing-infrastructure: add example test for module use case. Format cleanup.

--- __tests__/unit/media-play-button.spec.js
+++ __tests__/unit/media-play-button.spec.js
@@ -1,17 +1,16 @@
 import { spy } from 'sinon';
-import { fixture, expect } from "@open-wc/testing";
-import { MediaUIAttributes, MediaUIEvents } from "../../src/js/constants";
-import { verbs } from "../../src/js/labels/labels";
-import "../../src/js/media-play-button";
+import { fixture, expect } from '@open-wc/testing';
+import { MediaUIAttributes, MediaUIEvents } from '../../src/js/constants';
+import { verbs } from '../../src/js/labels/labels';
+import '../../src/js/media-play-button';
 
-describe("<media-play-button/>", () => {
-  it("passes the a11y audit", async () => {
+describe('<media-play-button/>', () => {
+  it('passes the a11y audit', async () => {
     const el = await fixture(`<media-play-button></media-play-button>`);
     await expect(el).shadowDom.to.be.accessible();
   });
 
   describe('default/playing', () => {
-
     let el;
 
     beforeEach(async () => {
@@ -22,116 +21,124 @@ describe("<media-play-button/>", () => {
       el = undefined;
     });
 
-    it("should expect pause actions", async () => {
+    it('should expect pause actions', async () => {
       expect(el.getAttribute('aria-label')).equals(verbs.PAUSE());
     });
 
-    it("should show pause", () => {
-      const icon = el.shadowRoot.querySelector('slot[name="pause"] > *')
+    it('should show pause', () => {
+      const icon = el.shadowRoot.querySelector('slot[name="pause"] > *');
       expect(icon).to.be.visible;
     });
 
     /** @TODO Figure out if this test failure is due to assertion assumptions of visible vs. invalid component initialization condition (CJP) */
-    it.skip("should not show play", () => {
-      const icon = el.shadowRoot.querySelector('slot[name="play"] > *')
+    it.skip('should not show play', () => {
+      const icon = el.shadowRoot.querySelector('slot[name="play"] > *');
       expect(icon).to.not.be.visible;
     });
 
-    it("should request pause when clicked", () => {
+    it('should request pause when clicked', () => {
       const requestHandler = spy();
       el.addEventListener(MediaUIEvents.MEDIA_PAUSE_REQUEST, requestHandler);
       el.click();
       expect(requestHandler.calledOnce).to.be.true;
     });
 
-    it("should not request play when clicked", () => {
+    it('should not request play when clicked', () => {
       const requestHandler = spy();
       el.addEventListener(MediaUIEvents.MEDIA_PLAY_REQUEST, requestHandler);
       el.click();
       expect(requestHandler.called).to.be.false;
     });
 
-    it.skip("should request pause when Enter pressed", () => {
+    it.skip('should request pause when Enter pressed', () => {
       /** @TODO Implement me (CJP) */
       expect(false).to.be.true;
     });
 
-    it.skip("should request pause when Space pressed", () => {
+    it.skip('should request pause when Space pressed', () => {
       /** @TODO Implement me (CJP) */
       expect(false).to.be.true;
     });
-
   });
 
   describe('paused', () => {
-
     let el;
 
     beforeEach(async () => {
-      el = await fixture(`<media-play-button ${MediaUIAttributes.MEDIA_PAUSED}></media-play-button>`);
+      el = await fixture(
+        `<media-play-button ${MediaUIAttributes.MEDIA_PAUSED}></media-play-button>`
+      );
     });
 
     afterEach(() => {
       el = undefined;
     });
 
-    it("should expect play actions", async () => {
+    it('should expect play actions', async () => {
       expect(el.getAttribute('aria-label')).equals(verbs.PLAY());
     });
 
-    it("should show play", () => {
-      const icon = el.shadowRoot.querySelector('slot[name="play"] > *')
+    it('should show play', () => {
+      const icon = el.shadowRoot.querySelector('slot[name="play"] > *');
       expect(icon).to.be.visible;
     });
 
     /** @TODO Figure out if this test failure is due to assertion assumptions of visible vs. invalid component initialization condition (CJP) */
-    it.skip("should not show pause", () => {
-      const icon = el.shadowRoot.querySelector('slot[name="pause"] > *')
+    it.skip('should not show pause', () => {
+      const icon = el.shadowRoot.querySelector('slot[name="pause"] > *');
       expect(icon).to.not.be.visible;
     });
 
-    it("should request play when clicked", () => {
+    it('should request play when clicked', () => {
       const requestHandler = spy();
       el.addEventListener(MediaUIEvents.MEDIA_PLAY_REQUEST, requestHandler);
       el.click();
       expect(requestHandler.calledOnce).to.be.true;
     });
 
-    it("should not request pause when clicked", () => {
+    it('should not request pause when clicked', () => {
       const requestHandler = spy();
       el.addEventListener(MediaUIEvents.MEDIA_PAUSE_REQUEST, requestHandler);
       el.click();
       expect(requestHandler.called).to.be.false;
     });
 
-    it.skip("should request play when Enter pressed", () => {
+    it.skip('should request play when Enter pressed', () => {
       /** @TODO Implement me (CJP) */
       expect(false).to.be.true;
     });
 
-    it.skip("should request play when Space pressed", () => {
+    it.skip('should request play when Space pressed', () => {
       /** @TODO Implement me (CJP) */
       expect(false).to.be.true;
     });
   });
 
   /** @TODO Investigate why slots are still svgs in these cases (CJP) */
-  describe.skip("custom slots", () => {
-    it("should support custom play content", async () => {
-      const idStr = "customPlay";
-      const slotStr = "play";
+  describe.skip('custom slots', () => {
+    it('should support custom play content', async () => {
+      const idStr = 'customPlay';
+      const slotStr = 'play';
 
-      const el = await fixture(`<media-play-button><span id="${idStr}" slot="${slotStr}">Play</span></media-play-button>`);
-      const slottedEl = el.shadowRoot.querySelector(`slot[name="${slotStr}"] > *`);
+      const el = await fixture(
+        `<media-play-button><span id="${idStr}" slot="${slotStr}">Play</span></media-play-button>`
+      );
+      const slottedEl = el.shadowRoot.querySelector(
+        `slot[name="${slotStr}"] > *`
+      );
       expect(slottedEl).to.have.id(idStr);
     });
 
-    it("should support custom pause content", async () => {
-      const idStr = "customPause";
-      const slotStr = "pause";
+    it('should support custom pause content', async () => {
+      const idStr = 'customPause';
+      const slotStr = 'pause';
 
-      const el = await fixture(`<media-play-button><span id="${idStr}" slot="${slotStr}">Play</span></media-play-button>`);
-      const slottedEl = el.shadowRoot.querySelector(`slot[name="${slotStr}"] > *`);
+      const el = await fixture(
+        `<media-play-button><span id="${idStr}" slot="${slotStr}">Play</span></media-play-button>`
+      );
+      const slottedEl = el.shadowRoot.querySelector(
+        `slot[name="${slotStr}"] > *`
+      );
       expect(slottedEl).to.have.id(idStr);
     });
   });
