bdd0543a228265bc9ba0387b009043ba08940a61
Christian Pillsbury <cjpillsbury@gmail.com>
2022-02-18 10:14:49 -0600

    fix/loading-indicator-svg-aria-hidden: aria-hide svg like we do other svgs (aria responsibilities belong to the elements).

--- src/js/media-loading-indicator.js
+++ src/js/media-loading-indicator.js
@@ -10,7 +10,7 @@ import {
 const template = document.createElement('template');
 
 const loadingIndicatorIcon = `
-<svg viewBox="0 0 100 100">
+<svg aria-hidden="true" viewBox="0 0 100 100">
   <path d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
     <animateTransform
        attributeName="transform"
