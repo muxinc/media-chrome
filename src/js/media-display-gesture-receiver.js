ac767acdc4218d42199e4abc6e1ebae2030c5f8a
Christian Pillsbury <cjpillsbury@gmail.com>
2022-05-04 15:14:42 -0500

    feat(media-play-gesture): clean up relationship between user-inactive and gestures. Clean up gestures-disabled functionality and slotted vs. 'default slotted' behavior.

---
+++ src/js/media-display-gesture-receiver.js
@@ -0,0 +1,142 @@
+import { MediaUIAttributes, MediaUIEvents, PointerTypes } from './constants.js';
+import { defineCustomElement } from './utils/defineCustomElement.js';
+import {
+  Window as window,
+  Document as document,
+} from './utils/server-safe-globals.js';
+
+const template = document.createElement('template');
+
+template.innerHTML = `
+<style>
+  :host {
+    display: inline-block;
+    width: auto;
+    height: auto;
+    box-sizing: border-box;
+
+    pointer-events: auto;
+  }
+</style>
+`;
+
+class MediaDisplayGestureReceiver extends window.HTMLElement {
+  // NOTE: Currently "baking in" actions + attrs until we come up with
+  // a more robust architecture (CJP)
+  static get observedAttributes() {
+    return [MediaUIAttributes.MEDIA_CONTROLLER, MediaUIAttributes.MEDIA_PAUSED];
+  }
+
+  constructor(options = {}) {
+    super();
+
+    const shadow = this.attachShadow({ mode: 'open' });
+
+    const buttonHTML = template.content.cloneNode(true);
+    this.nativeEl = buttonHTML;
+
+    // Slots
+    let slotTemplate = options.slotTemplate;
+
+    if (!slotTemplate) {
+      slotTemplate = document.createElement('template');
+      slotTemplate.innerHTML = `<slot>${options.defaultContent || ''}</slot>`;
+    }
+
+    this.nativeEl.appendChild(slotTemplate.content.cloneNode(true));
+
+    shadow.appendChild(buttonHTML);
+
+    this._pointerType = undefined;
+    const pointerDownHandler = (e) => {
+      // Since not all browsers have updated to be spec compliant, where 'click' events should be PointerEvents,
+      // we can use use 'pointerdown' to reliably determine the pointer type. (CJP).
+      this._pointerType = e.pointerType;
+    };
+
+    this.addEventListener('pointerdown', pointerDownHandler);
+
+    this.addEventListener('click', (event) => {
+      if (!event.pointerType) {
+        event.pointerType = this._pointerType;
+      }
+      const { pointerType } = event;
+      // NOTE: While there are cases where we may have a stale this._pointerType,
+      // we're guaranteed that the most recent this._pointerType will correspond
+      // to the current click event definitionally. As such, this clearing is technically
+      // unnecessary (CJP)
+      this._pointerType = undefined;
+
+      // NOTE: Longer term, we'll likely want to delay this to support double click/double tap (CJP)
+      if (pointerType === PointerTypes.TOUCH) {
+        this.handleTap(event);
+        return;
+      } else if (pointerType === PointerTypes.MOUSE) {
+        this.handleMouseClick(event);
+        return;
+      } else if (pointerType === PointerTypes.PEN) {
+        this.handlePenTap(event);
+        return;
+      }
+    });
+  }
+
+  attributeChangedCallback(attrName, oldValue, newValue) {
+    if (attrName === MediaUIAttributes.MEDIA_CONTROLLER) {
+      if (oldValue) {
+        const mediaControllerEl = document.getElementById(oldValue);
+        mediaControllerEl?.unassociateElement?.(this);
+      }
+      if (newValue) {
+        const mediaControllerEl = document.getElementById(newValue);
+        mediaControllerEl?.associateElement?.(this);
+      }
+    }
+  }
+
+  connectedCallback() {
+    this.setAttribute('tabindex', -1);
+    this.setAttribute('aria-hidden', true);
+
+    const mediaControllerId = this.getAttribute(
+      MediaUIAttributes.MEDIA_CONTROLLER
+    );
+    if (mediaControllerId) {
+      const mediaControllerEl = document.getElementById(mediaControllerId);
+      mediaControllerEl?.associateElement?.(this);
+    }
+  }
+
+  disconnectedCallback() {
+    const mediaControllerSelector = this.getAttribute(
+      MediaUIAttributes.MEDIA_CONTROLLER
+    );
+    if (mediaControllerSelector) {
+      const mediaControllerEl = document.getElementById(mediaControllerId);
+      mediaControllerEl?.unassociateElement?.(this);
+    }
+  }
+
+  // NOTE: Currently "baking in" actions + attrs until we come up with
+  // a more robust architecture (CJP)
+  handleTap(_evt) {}
+
+  handlePenTap(_evt) {}
+
+  handleMouseClick(_evt) {
+    const eventName =
+      this.getAttribute(MediaUIAttributes.MEDIA_PAUSED) != null
+        ? MediaUIEvents.MEDIA_PLAY_REQUEST
+        : MediaUIEvents.MEDIA_PAUSE_REQUEST;
+    this.dispatchEvent(
+      new window.CustomEvent(eventName, { composed: true, bubbles: true })
+    );
+  }
+}
+
+defineCustomElement(
+  'media-display-gesture-receiver',
+  MediaDisplayGestureReceiver
+);
+
+export default MediaDisplayGestureReceiver;
