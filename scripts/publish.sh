2b6bb9930099440723d8d02e5606bc3efc2926e5
Wesley Luyten <me@wesleyluyten.com>
2022-06-22 09:50:28 -0500

    ci: improve publish release script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,16 +3,39 @@
 # npm publish with goodies
 # inspired by https://gist.github.com/stevemao/280ef22ee861323993a0
 #
-# prerequisites:
-# `npm install -D conventional-recommended-bump \
-#   conventional-changelog-cli conventional-github-releaser`
-# `release` with optional argument `patch`/`minor`/`major`/`<version>`
+# release with optional argument `patch`/`minor`/`major`/`canary`/`<version>`
 # defaults to conventional-recommended-bump
 
-# `np` is not suitable for CI/CD
-# https://github.com/sindresorhus/np/issues/619#issuecomment-994493179
+function main {
+  processCommandLineArgs "$@"
+  release "$@"
+}
 
-release() {
+function processCommandLineArgs {
+  for arg in "$@"
+  do
+    case $arg in
+      canary)
+        canary
+        exit 0
+        ;;
+      --help|help)
+        echo "Commands:"
+        echo "  $0            Publish a release from conventional commits."
+        echo "  $0 canary     Publish a canary."
+        echo "  $0 patch      Publish a patch release."
+        echo "  $0 minor      Publish a minor release."
+        echo "  $0 major      Publish a major release."
+        echo "  $0 <version>  Publish a release with a specific version."
+        exit 0
+        ;;
+      *)
+        ;;
+    esac
+  done
+}
+
+function release {
   BUMP=$(npx -p conventional-changelog-angular -p conventional-recommended-bump -c 'conventional-recommended-bump -p angular')
   VERSION=$(npm --no-git-tag-version version ${1:-$BUMP})
   npx conventional-changelog-cli -p angular -i CHANGELOG.md -s
@@ -24,9 +47,9 @@ release() {
   npm publish
 };
 
-canary() {
+function canary {
   # get last published version from NPM without alpha / beta, remove -SHA hash
-  LAST_VERSION=$(npm view media-chrome versions --json |
+  LAST_VERSION=$(npm view $(npm pkg get name | sed 's/"//g') versions --json |
     jq -r '. - map(select(contains("alpha") or contains("beta"))) | last' |
     sed -r 's/-[a-z0-9]{7}$//g')
   PRE_VERSION=$(npx semver $LAST_VERSION -i prerelease --preid canary)
@@ -34,3 +57,5 @@ canary() {
   npm --no-git-tag-version version $VERSION
   npm publish --tag canary
 }
+
+main "$@"
