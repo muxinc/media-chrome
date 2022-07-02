9a32cb89655fdd509d34f693d6492e5ee2b964af
Wesley Luyten <me@wesleyluyten.com>
2022-07-01 19:26:46 -0500

    ci: improve publish release script (#254)

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,34 +3,59 @@
 # npm publish with goodies
 # inspired by https://gist.github.com/stevemao/280ef22ee861323993a0
 #
-# prerequisites:
-# `npm install -D conventional-recommended-bump \
-#   conventional-changelog-cli conventional-github-releaser`
-# `release` with optional argument `patch`/`minor`/`major`/`<version>`
+# release with optional argument `patch`/`minor`/`major`/`canary`/`<version>`
 # defaults to conventional-recommended-bump
 
-# `np` is not suitable for CI/CD
-# https://github.com/sindresorhus/np/issues/619#issuecomment-994493179
+function main {
+  processCommandLineArgs "$@"
+  release "$@"
+}
 
-release() {
-  BUMP=$(conventional-recommended-bump -p angular)
+function processCommandLineArgs {
+  for arg in "$@"
+  do
+    case $arg in
+      canary)
+        canary
+        exit 0
+        ;;
+      --help|help)
+        echo "Commands:"
+        echo "  $0            Publish a release from conventional commits."
+        echo "  $0 canary     Publish a canary."
+        echo "  $0 patch      Publish a patch release."
+        echo "  $0 minor      Publish a minor release."
+        echo "  $0 major      Publish a major release."
+        echo "  $0 <version>  Publish a release with a specific version."
+        exit 0
+        ;;
+      *)
+        ;;
+    esac
+  done
+}
+
+function release {
+  BUMP=$(npx -p conventional-changelog-angular -p conventional-recommended-bump -c 'conventional-recommended-bump -p angular')
   VERSION=$(npm --no-git-tag-version version ${1:-$BUMP})
-  conventional-changelog -p angular -i CHANGELOG.md -s
+  npx conventional-changelog-cli -p angular -i CHANGELOG.md -s
   git add CHANGELOG.md
   git commit -m "docs(CHANGELOG): $VERSION"
   npm --force --allow-same-version version $VERSION -m "chore(release): %s"
   git push --follow-tags
-  conventional-github-releaser -p angular
+  npx conventional-github-releaser -p angular
   npm publish
 };
 
-canary() {
+function canary {
   # get last published version from NPM without alpha / beta, remove -SHA hash
-  LAST_VERSION=$(npm view media-chrome versions --json |
+  LAST_VERSION=$(npm view $(npm pkg get name | sed 's/"//g') versions --json |
     jq -r '. - map(select(contains("alpha") or contains("beta"))) | last' |
     sed -r 's/-[a-z0-9]{7}$//g')
-  PRE_VERSION=$(semver $LAST_VERSION -i prerelease --preid canary)
+  PRE_VERSION=$(npx semver $LAST_VERSION -i prerelease --preid canary)
   VERSION=$PRE_VERSION-$(git rev-parse --short HEAD)
   npm --no-git-tag-version version $VERSION
   npm publish --tag canary
 }
+
+main "$@"
