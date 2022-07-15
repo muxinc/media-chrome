53302c8923b9a073db20af217e4229f73bac5087
Wesley Luyten <wesley@mux.com>
2022-07-15 13:06:08 -0500

    chore: make yarn dev auto get free port

--- scripts/esbuild.js
+++ scripts/esbuild.js
@@ -6,65 +6,67 @@ import os from 'os';
 
 const interfaces = os.networkInterfaces();
 
-console.log('');
-console.log(` > Local:   http://127.0.0.1:8000/examples/`);
-console.log(` > Network: http://${getNetworkAddress()}:8000/examples/`);
+async function serve() {
+  const port = await getFreePort(8000);
 
-// Start esbuild's server on local port 8010
-esbuild
-  .serve(
-    {
-      servedir: '.',
-      port: 8010,
-    },
-    {
-      entryPoints: [...walkSync('src/js/')].filter((entry) =>
-        entry.endsWith('.js')
-      ),
-      outdir: 'dist',
-      target: 'es2019',
-    }
-  )
-  .then((result) => {
-    // The result tells us where esbuild's local server is
-    const { host, port } = result;
+  console.log('');
+  console.log(` > Local:   http://localhost:${port}/examples/`);
+  console.log(` > Network: http://${getNetworkAddress()}:${port}/examples/`);
 
-    // Then start a proxy server on port 8000
-    http
-      .createServer((req, res) => {
-        const options = {
-          hostname: host,
-          port: port,
-          path: req.url,
-          method: req.method,
-          headers: req.headers,
-        };
+  // Start esbuild's server on local port 8010
+  esbuild
+    .serve(
+      {
+        servedir: '.',
+        port: 0, // port zero makes it find an unused port
+      },
+      {
+        entryPoints: [...walkSync('src/js/')].filter((entry) =>
+          entry.endsWith('.js')
+        ),
+        outdir: 'dist',
+        target: 'es2019',
+      }
+    )
+    .then((result) => {
+      // The result tells us where esbuild's local server is
+      // Then start a proxy server on port 8000
+      http
+        .createServer((req, res) => {
+          const options = {
+            hostname: result.host,
+            port: result.port,
+            path: req.url,
+            method: req.method,
+            headers: req.headers,
+          };
 
-        // Forward each incoming request to esbuild
-        const proxyReq = http.request(options, (proxyRes) => {
-          // If esbuild returns "not found", send a custom 404 page
-          if (proxyRes.statusCode === 404) {
-            res.writeHead(404, { 'Content-Type': 'text/html' });
-            res.end('<h1>404 Page Not Found</h1>');
-            return;
-          }
+          // Forward each incoming request to esbuild
+          const proxyReq = http.request(options, (proxyRes) => {
+            // If esbuild returns "not found", send a custom 404 page
+            if (proxyRes.statusCode === 404) {
+              res.writeHead(404, { 'Content-Type': 'text/html' });
+              res.end('<h1>404 Page Not Found</h1>');
+              return;
+            }
 
-          if (proxyRes.req.path === '/') {
-            res.writeHead(302, { Location: '/examples' });
-            res.end();
-            return;
-          }
+            if (proxyRes.req.path === '/') {
+              res.writeHead(302, { Location: '/examples' });
+              res.end();
+              return;
+            }
 
-          // Otherwise, forward the response from esbuild to the client
-          res.writeHead(proxyRes.statusCode, proxyRes.headers);
-          proxyRes.pipe(res, { end: true });
-        });
+            // Otherwise, forward the response from esbuild to the client
+            res.writeHead(proxyRes.statusCode, proxyRes.headers);
+            proxyRes.pipe(res, { end: true });
+          });
 
-        // Forward the body of the request to esbuild
-        req.pipe(proxyReq, { end: true });
-      })
-      .listen(8000);
-  });
+          // Forward the body of the request to esbuild
+          req.pipe(proxyReq, { end: true });
+        })
+        .listen(port);
+    });
+}
 
 function* walkSync(dir) {
   const files = fs.readdirSync(dir, { withFileTypes: true });
@@ -87,3 +89,23 @@ function getNetworkAddress() {
     }
   }
 }
+
+async function getFreePort(base = 8000) {
+  for (let port = base; port < base + 100; port++) {
+    if (await isPortAvailable(port)) return port;
+  }
+}
+
+function isPortAvailable(port) {
+  return new Promise((resolve) => {
+    const tester = http
+      .createServer()
+      .once('error', () => resolve(false))
+      .once('listening', () =>
+        tester.once('close', () => resolve(true)).close()
+      )
+      .listen(port);
+  });
+}
+
+serve();
