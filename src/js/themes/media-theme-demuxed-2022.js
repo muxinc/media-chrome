2c6c762125dbdbe63db54156d338f81c0b1c77a3
Wietse Hage <wietsehage@iMac-van-Wietse.local>
2022-10-01 13:32:36 +0200

    Rename, button class and Airplay / Cast buttons

--- src/js/themes/media-theme-demuxed-2022.js
+++ src/js/themes/media-theme-demuxed-2022.js
@@ -1,10 +1,10 @@
 /* 
-<media-theme-demuxed>
+<media-theme-demuxed-2022>
   <video
     slot="media"
     src="https://stream.mux.com/DS00Spx1CV902MCtPj5WknGlR102V5HFkDe/high.mp4"
   ></video>
-</media-theme-demuxed>
+</media-theme-demuxed-2022>
 */
 
 import { defineCustomElement } from '../utils/defineCustomElement.js';
@@ -18,8 +18,16 @@ const template = `
     --tertiary-color: #7596CC;
   }
 
-  video {
-    object-fit: initial;
+  media-airplay-button[media-airplay-unavailable] {
+    display: none;
+  }
+
+  media-cast-button[media-cast-unavailable] {
+    display: none;
+  }
+
+  media-pip-button[media-pip-unavailable] {
+    display: none;
   }
 
   media-controller {
@@ -56,7 +64,7 @@ const template = `
     margin: 0 0 0 5px;
   }
 
-  media-control-bar media-play-button, media-mute-button, media-seek-forward-button, media-captions-button, media-pip-button, media-fullscreen-button, media-settings-button, media-cinema-button {
+  .small-button {
     position: relative;
     flex: none;
     margin: 0 5px;
@@ -69,15 +77,24 @@ const template = `
     border-radius: 9999px;
   }
 
-  media-control-bar media-play-button:hover, media-mute-button:hover, media-seek-forward-button:hover, media-captions-button:hover, media-pip-button:hover, media-fullscreen-button:hover, media-settings-button:hover, media-cinema-button:hover {
+  .small-button svg {
+    position: absolute;
+    overflow: hidden;
+    width: 100%;
+    height: 20px;
+    margin: 0 !important;
+    padding: 0 !important;
+  }
+
+  .small-button:hover {
     box-shadow: 0 0 0 calc(2px) var(--tertiary-color);
   }
 
-  span media-play-button {
+  div[slot="centered-chrome"] media-play-button {
     position: relative;
     flex: none;
-    margin: 0 5px;
     display: flex;
+    margin-bottom: 10px;
     align-items: center;
     justify-items: center;
     height: 96px;
@@ -86,26 +103,17 @@ const template = `
     border-radius: 9999px;
   }
 
-  span media-play-button:hover {
+  div[slot="centered-chrome"] media-play-button:hover {
     box-shadow: 0 0 0 calc(2px) var(--tertiary-color);
   }
 
-  span media-play-button svg {
+  div[slot="centered-chrome"] media-play-button svg {
     filter: invert(100%);
     height: 64px;
   }
 
-  media-settings-button, media-cinema-button {
+  media-cinema-button {
     cursor: pointer;
-  }
-
-  media-control-bar svg {
-    position: absolute;
-    overflow: hidden;
-    width: 100%;
-    height: 20px;
-    margin: 0 !important;
-    padding: 0 !important;
   }
 
   media-time-range {
@@ -138,6 +146,17 @@ const template = `
     background-image: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAACqCAYAAABsziWkAAAAAXNSR0IArs4c6QAAAQVJREFUOE9lyNdHBQAAhfHb3nvvuu2997jNe29TJJEkkkgSSSSJJJJEEkkiifRH5jsP56Xz8PM5gcC/xfDEmjhKxEOCSaREEiSbFEqkQppJpzJMJiWyINvkUCIX8kw+JQqg0BRRxaaEEqVQZsopUQGVpooS1VBjglStqaNEPTSYRko0QbNpoUQrtJl2qsN0UqILuk0PJXqhz/RTYgAGzRA1bEYoMQpjZpwSExAyk5SYgmkzQ82aOUqEIWKilJiHBbNIiSVYhhVYhTVYhw3YhC3Yhh3YhT3YhwM4hCM4hhM4hTM4hwu4hCu4hhu4hTu4hwd4hCd4hhd4hTd4hw/4hC/4hh/4/QM2/id28uIEJAAAAABJRU5ErkJggg==");
   }
 
+  div[slot="top-chrome"] {
+    width: calc(100% - 32px);
+    display: flex;
+    flex-direction: row-reverse;
+    padding-right: 32px;
+  }
+
+  div[slot="top-chrome"] .small-button {
+    margin: 32px 8px;
+  }
+
   @media (max-width: 720px) {
     media-control-bar {
       background: transparent;
@@ -147,11 +166,12 @@ const template = `
       align-items: flex-start;
     }
 
-    media-control-bar media-play-button, media-mute-button, media-seek-forward-button, media-captions-button, media-pip-button, media-settings-button, media-cinema-button {
+    .small-button {
       display: none;
     }
 
-    media-fullscreen-button {
+    media-fullscreen-button.small-button {
+      display: flex;
       position: absolute;
       top: 8px;
       right: 20px;
@@ -165,13 +185,13 @@ const template = `
       padding: 0 10px;
     }
 
-    span media-play-button {
+    div[slot="centered-chrome"] media-play-button {
       z-index: 1;
       height: 72px;
       width: 72px;
     }
 
-    span media-play-button svg {
+    div[slot="centered-chrome"] media-play-button svg {
       height: 44px;
     }
   }
@@ -181,7 +201,23 @@ const template = `
   <slot name="media" slot="media"></slot>
   <slot name="poster" slot="poster"></slot>
   <div class="demuxed-gradient-bottom"></div>
-  <span slot="centered-chrome">
+  <div slot="top-chrome">
+    <media-cast-button class="small-button">
+      <svg slot="enter" viewBox="0 0 26 26">
+        <g transform="translate(1.5, 0.5)">
+          <path d="M1,18 L1,21 L4,21 C4,19.3 2.66,18 1,18 L1,18 Z"/><path d="M1,14 L1,16 C3.76,16 6,18.2 6,21 L8,21 C8,17.13 4.87,14 1,14 L1,14 Z"/><path class="cast_caf_icon_arch2" d="M1,10 L1,12 C5.97,12 10,16.0 10,21 L12,21 C12,14.92 7.07,10 1,10 L1,10 Z"/><path d="M21,3 L3,3 C1.9,3 1,3.9 1,5 L1,8 L3,8 L3,5 L21,5 L21,19 L14,19 L14,21 L21,21 C22.1,21 23,20.1 23,19 L23,5 C23,3.9 22.1,3 21,3 L21,3 Z"/></g>
+        </g>
+      </svg>
+    </media-cast-button>
+    <media-airplay-button class="small-button">
+      <svg slot="airplay" viewBox="0 0 24 24">
+        <g transform="translate(-0.7, 0.5)">
+          <path d="M22.13 3H3.87a.87.87 0 0 0-.87.87v13.26a.87.87 0 0 0 .87.87h3.4L9 16H5V5h16v11h-4l1.72 2h3.4a.87.87 0 0 0 .87-.87V3.87a.87.87 0 0 0-.86-.87Zm-8.75 11.44a.5.5 0 0 0-.76 0l-4.91 5.73a.5.5 0 0 0 .38.83h9.82a.501.501 0 0 0 .38-.83l-4.91-5.73Z"/>
+        </g>
+      </svg>
+    </media-airplay-button>
+  </div>
+  <div slot="centered-chrome">
     <media-play-button>
       <svg slot="play" viewBox="0 0 16 16">
         <g transform="translate(-6, -6)">
@@ -199,9 +235,9 @@ const template = `
         </g>
       </svg>
     </media-play-button>
-  </span>
+  </div>
   <media-control-bar>
-    <media-play-button>
+    <media-play-button class="small-button">
       <svg slot="play" viewBox="0 0 16 16">
         <g transform="translate(-6, -6)">
           <path d="M19.6,13.2L11.1,9c-0.6-0.3-1.2,0.1-1.2,0.7v8.5c0,0.6,0.7,1,1.2,0.7l8.5-4.2C20.2,14.4,20.2,13.6,19.6,13.2z"/>
@@ -218,7 +254,7 @@ const template = `
         </g>
       </svg>
     </media-play-button>
-    <media-mute-button>
+    <media-mute-button class="small-button">
       <svg slot="off" viewBox="0 0 16 16">
         <g transform="translate(-6, -6)">
           <path d="M13,8.2l-2.8,2.9v0.1H7.4c-0.5,0-0.9,0.4-0.9,0.9V16c0,0.5,0.4,0.9,0.9,0.9h2.8l2.8,2.9c0.3,0.3,0.8,0.1,0.8-0.3v-11
@@ -247,7 +283,7 @@ const template = `
         </g>
       </svg>
     </media-mute-button>
-    <media-seek-forward-button>
+    <media-seek-forward-button class="small-button">
       <svg slot="forward" viewBox="0 0 16 16">
         <g transform="translate(-6, -6)">
           <g>
@@ -258,13 +294,13 @@ const template = `
           <path d="M18.8,19.1L18.8,19.1c-0.5,0-0.8-0.4-0.8-0.8V9.7c0-0.5,0.4-0.8,0.8-0.8l0,0c0.5,0,0.8,0.4,0.8,0.8v8.6C19.7,18.7,19.3,19.1,18.8,19.1z"/>
         </g>
       </svg>
-    </media-seek-forward-button>
+    </media-seek-forward-button class="small-button">
     <media-time-display show-duration></media-time-display>
     <media-time-range>
       <media-preview-thumbnail slot="preview"></media-preview-thumbnail>
       <media-preview-time-display slot="preview"></media-preview-time-display>
     </media-time-range>
-    <media-captions-button>
+    <media-captions-button class="small-button">
       <svg slot="off" viewBox="0 0 16 16">
         <style type="text/css">
           .st0{fill:#FFFFFF;}
@@ -313,9 +349,9 @@ const template = `
           </g>
         </g>
       </svg>
-    </media-captions-button>
+    </media-captions-button class="small-button">
     <!--
-    <media-settings-button>
+    <media-settings-button class="small-button">
       <svg viewBox="0 0 16 16">
         <style type="text/css">
           .st0{fill:#FFFFFF;}
@@ -326,7 +362,7 @@ const template = `
       </svg>
     </media-settings-button>
     -->
-    <media-pip-button>
+    <media-pip-button class="small-button">
       <svg slot="enter" viewBox="0 0 16 16">
         <style type="text/css">
           .st0{fill:#FFFFFF;}
@@ -350,7 +386,7 @@ const template = `
         </g>
       </svg>
     </media-pip-button>
-    <media-cinema-button>
+    <media-cinema-button class="small-button">
       <svg viewBox="0 0 16 16">
         <style type="text/css">
           .st0{fill:#FFFFFF;}
@@ -360,7 +396,7 @@ const template = `
         </g>
       </svg>
     </media-cinema-button>
-    <media-fullscreen-button>
+    <media-fullscreen-button class="small-button">
       <svg slot="enter" viewBox="0 0 16 16">
         <style type="text/css">
           .st0{fill:#FFFFFF;}
@@ -408,6 +444,6 @@ class MediaThemeDemuxed extends MediaTheme {
   static template = template;
 }
 
-defineCustomElement('media-theme-demuxed', MediaThemeDemuxed);
+defineCustomElement('media-theme-demuxed-2022', MediaThemeDemuxed);
 
 export default MediaThemeDemuxed;
