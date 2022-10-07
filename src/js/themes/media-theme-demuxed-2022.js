e415646898cd96548e4dc30af4ecf50938bdddd9
wietsehage <wietse@wietsehage.nl>
2022-10-07 12:52:59 +0200

    Improving accessibility with screen readers

--- src/js/themes/media-theme-demuxed-2022.js
+++ src/js/themes/media-theme-demuxed-2022.js
@@ -208,6 +208,18 @@ const template = `
     display: flex;
   }
 
+  media-controller[user-inactive]:not(.sm) * {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    white-space: nowrap;
+    border-width: 0;
+  }
+
   media-controller:not(.sm) media-control-bar {
     position: static;
     background: transparent;
