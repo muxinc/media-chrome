67b389eaed575e5a20e2f0fb113af9c691297ea7
Wesley Luyten <wesley@mux.com>
2023-01-05 15:12:32 -0600

    fix: add greater than, less than operators

--- test/unit/template-processor.spec.js
+++ test/unit/template-processor.spec.js
@@ -1,7 +1,7 @@
 import { assert } from '@open-wc/testing';
-import { evaluateCondition } from '../../src/js/utils/template-processor.js';
+import { evaluateCondition, getParamValue } from '../../src/js/utils/template-processor.js';
 
-describe('template processor', () => {
+describe('evaluateCondition', () => {
   it('can evaluate a simple boolean condition', () => {
     assert(evaluateCondition('true'));
     assert(evaluateCondition('true == myVar', { myVar: true }));
@@ -11,6 +11,12 @@ describe('template processor', () => {
   it('can evaluate a simple number condition', async () => {
     assert(!evaluateCondition('0'));
     assert(evaluateCondition('1'));
+    assert(evaluateCondition('5 > 3'));
+    assert(evaluateCondition('5 >= 3'));
+    assert(evaluateCondition('5 >= 5'));
+    assert(evaluateCondition('2 < 3'));
+    assert(evaluateCondition('2 <= 3'));
+    assert(evaluateCondition('3 <= 3'));
     assert(evaluateCondition('myVar == 22', { myVar: 22 }));
     assert(evaluateCondition('myVar != 22', { myVar: 23 }));
   });
@@ -20,5 +26,16 @@ describe('template processor', () => {
     assert(evaluateCondition('"thruthy"'));
     assert(evaluateCondition('myVar == "a string"', { myVar: 'a string' }));
     assert(evaluateCondition('myVar != "a string"', { myVar: 'lalala' }));
+  });
+});
+
+describe('getParamValue', () => {
+  it('gets the correct value from string', () => {
+    assert.equal(getParamValue('true'), true);
+    assert.equal(getParamValue('false'), false);
+    assert.equal(getParamValue('"hello world"'), 'hello world');
+    assert.equal(getParamValue("'hello world'"), 'hello world');
+    assert.equal(getParamValue('360'), 360);
+    assert.equal(getParamValue('myVar', { myVar: 'val' }), 'val');
   });
 });
