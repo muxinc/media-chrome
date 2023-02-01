3ddf3a904ea6a0fb250253b6c035323e280713fc
Gary Katsevman <git@gkatsev.com>
2023-02-01 16:16:29 -0500

    fix(experimental): move media-captions-menu-button to experimental folder (#464)

--- src/js/experimental/media-captions-listbox.js
+++ src/js/experimental/media-captions-listbox.js
@@ -1,9 +1,9 @@
 import MediaChromeListbox from './media-chrome-listbox.js';
 import './media-chrome-listitem.js';
-import { window, document } from './utils/server-safe-globals.js';
-import { MediaUIAttributes, MediaUIEvents } from './constants.js';
-import { parseTextTracksStr, formatTextTrackObj } from './utils/captions.js';
-import { toggleSubsCaps } from './utils/captions.js';
+import { window, document } from '../utils/server-safe-globals.js';
+import { MediaUIAttributes, MediaUIEvents } from '../constants.js';
+import { parseTextTracksStr, formatTextTrackObj } from '../utils/captions.js';
+import { toggleSubsCaps } from '../utils/captions.js';
 
 const captionsIndicatorInlineStyle = `
   fill: var(--media-icon-color, #eee);
