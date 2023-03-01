c64a514a947cd80009f73cf334e2aed3b1a465e7
Wesley Luyten <me@wesleyluyten.com>
2023-03-01 10:11:30 -0600

    fix: fix nullish coalesce operator, improve proces

--- test/unit/template-processor.spec.js
+++ test/unit/template-processor.spec.js
@@ -1,32 +1,56 @@
 import { assert } from '@open-wc/testing';
-import { evaluateCondition, getParamValue, processor } from '../../src/js/utils/template-processor.js';
+import { evaluateExpression, getParamValue, processor } from '../../src/js/utils/template-processor.js';
 import { TemplateInstance } from '../../src/js/utils/template-parts.js';
 
-describe('evaluateCondition', () => {
+describe('evaluateExpression', () => {
   it('can evaluate a simple boolean condition', () => {
-    assert(evaluateCondition('true'));
-    assert(evaluateCondition('true == myVar', { myVar: true }));
-    assert(evaluateCondition('myVar != false', { myVar: true }));
+    assert(evaluateExpression('true'));
+    assert(evaluateExpression('true == myVar', { myVar: true }));
+    assert(evaluateExpression('myVar != false', { myVar: true }));
   });
 
   it('can evaluate a simple number condition', async () => {
-    assert(!evaluateCondition('0'));
-    assert(evaluateCondition('1'));
-    assert(evaluateCondition('5 > 3'));
-    assert(evaluateCondition('5 >= 3'));
-    assert(evaluateCondition('5 >= 5'));
-    assert(evaluateCondition('2 < 3'));
-    assert(evaluateCondition('2 <= 3'));
-    assert(evaluateCondition('3 <= 3'));
-    assert(evaluateCondition('myVar == 22', { myVar: 22 }));
-    assert(evaluateCondition('myVar != 22', { myVar: 23 }));
+    assert(!evaluateExpression('0'));
+    assert(evaluateExpression('1'));
+    assert(evaluateExpression('5 > 3'));
+    assert(evaluateExpression('5 >= 3'));
+    assert(evaluateExpression('5 >= 5'));
+    assert(evaluateExpression('2 < 3'));
+    assert(evaluateExpression('2 <= 3'));
+    assert(evaluateExpression('3 <= 3'));
+    assert(evaluateExpression('myVar == 22', { myVar: 22 }));
+    assert(evaluateExpression('myVar != 22', { myVar: 23 }));
   });
 
   it('can evaluate a simple string condition', async () => {
-    assert(!evaluateCondition('""'));
-    assert(evaluateCondition('"thruthy"'));
-    assert(evaluateCondition('myVar == "a string"', { myVar: 'a string' }));
-    assert(evaluateCondition('myVar != "a string"', { myVar: 'lalala' }));
+    assert(!evaluateExpression('""'));
+    assert(evaluateExpression('"thruthy"'));
+    assert(evaluateExpression('myVar == "a string"', { myVar: 'a string' }));
+    assert(evaluateExpression('myVar != "a string"', { myVar: 'lalala' }));
+  });
+
+  it('can evaluate the string filter', () => {
+    assert.equal(evaluateExpression('2.34 | string'), '2.34');
+  });
+
+  it('can evaluate nullish coalesce operator', () => {
+    assert.equal(evaluateExpression('myVar ?? "on-demand"'), 'on-demand');
+    assert.equal(evaluateExpression('myVar ?? "hello world"'), 'hello world');
+    assert.equal(evaluateExpression('myVar ?? 99'), 99);
+    assert.equal(evaluateExpression('myVar ?? true'), true);
+    assert.equal(evaluateExpression('null ?? true'), true);
+    assert.equal(evaluateExpression('undefined ?? true'), true);
+  });
+
+  it('can evaluate partial', () => {
+    const MyButton = {};
+    let result = evaluateExpression('>MyButton arg1="hello"', { MyButton });
+    assert.equal(result, MyButton);
+    assert.deepEqual(result.state, { MyButton, arg1: 'hello' });
+
+    result = evaluateExpression(' > MyButton ', { MyButton });
+    assert.equal(result, MyButton);
+    assert.deepEqual(result.state, { MyButton });
   });
 });
 
@@ -35,9 +59,12 @@ describe('getParamValue', () => {
     assert.equal(getParamValue('true'), true);
     assert.equal(getParamValue('false'), false);
     assert.equal(getParamValue('"hello world"'), 'hello world');
+    assert.equal(getParamValue('"hello-world"'), 'hello-world');
+    assert.equal(getParamValue('"hello_world"'), 'hello_world');
     assert.equal(getParamValue("'hello world'"), 'hello world');
     assert.equal(getParamValue('360'), 360);
     assert.equal(getParamValue('myVar', { myVar: 'val' }), 'val');
+    assert.equal(getParamValue('my_var', { my_var: 'val' }), 'val');
   });
 });
 
