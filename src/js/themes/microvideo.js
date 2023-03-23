6ea607921bf682269bd963001f35fa5ccca6506e
Wesley Luyten <me@wesleyluyten.com>
2023-03-23 14:44:47 -0500

    fix: add poster slot, media loading indicator to Microvideo (#505)
    * fix: add poster slot, media loading indicator
    
    * fix: use html comment for syntax highlighting
    https://github.com/sublimehq/Packages/issues/179
    
    * example: update Microvideo theme in experimental

--- src/js/themes/microvideo.js
+++ src/js/themes/microvideo.js
@@ -10,10 +10,8 @@
 import { window, document } from '../utils/server-safe-globals.js';
 import { MediaThemeElement } from '../media-theme-element.js';
 
-const html = (raw, ...keys) => String.raw({ raw }, ...keys);
-
 const template = document.createElement('template');
-template.innerHTML = html`
+template.innerHTML = /*html*/`
 <style>
   :host {
     --_primary-color: var(--primary-color, #fff);
@@ -75,8 +73,15 @@ template.innerHTML = html`
     padding-bottom: 0;
   }
 
+  media-loading-indicator {
+    place-self: center;
+    ${/* Stack the grid items on top of each other */''}
+    grid-area: 1 / 1;
+  }
+
   media-control-bar {
     place-self: var(--_control-bar-place-self, end center);
+    grid-area: 1 / 1;
     position: relative;
     margin: 10px;
     gap: 4px;
@@ -221,7 +226,7 @@ template.innerHTML = html`
     width: 7px;
   }
 
-  /* Turn some buttons off by default */
+  ${/* Turn some buttons off by default */''}
   media-seek-backward-button {
     display: var(--media-control-display, var(--media-seek-backward-button-display, none));
   }
@@ -424,8 +429,17 @@ template.innerHTML = html`
   </span>
 </template>
 
-<media-controller style="--_control-bar-place-self:{{controlBarPlace ?? 'unset'}}">
+<media-controller
+  style="--_control-bar-place-self:{{controlBarPlace ?? 'unset'}}"
+  gestures-disabled="{{disabled}}"
+  hotkeys="{{hotkeys}}"
+  nohotkeys="{{nohotkeys}}"
+  audio="{{audio}}"
+  exportparts="layer, media-layer, poster-layer, vertical-layer, centered-layer, gesture-layer"
+>
   <slot name="media" slot="media"></slot>
+  <slot name="poster" slot="poster"></slot>
+  <media-loading-indicator slot="centered-chrome" no-auto-hide></media-loading-indicator>
 
   <template if="streamType == 'on-demand'">
 
