f31d5a3b7bf28ae92b4b833747ad22d235e9dac5
Wesley Luyten <me@wesleyluyten.com>
2023-04-12 13:36:23 -0500

    fix: font numeric uniform width (#526)
    * fix: font numeric uniform width
    
    * fix: add SegoeUI > Windows, add Roboto > Android

--- src/js/experimental/media-chrome-listbox.js
+++ src/js/experimental/media-chrome-listbox.js
@@ -14,7 +14,7 @@ template.innerHTML = `
     padding: 0.5em;
     background-color: var(--media-listbox-background, var(--media-control-background, rgba(10,10,15, .8)));
     color: var(--media-text-color, white);
-    font-family: Arial, sans-serif;
+    font-family: helvetica neue, segoe ui, roboto, arial, sans-serif;
   }
 
   ::slotted(media-chrome-listitem[tabindex="0"]:focus-visible),
