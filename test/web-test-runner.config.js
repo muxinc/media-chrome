ac90e2ebc1db05bb4ce6be4c286a90ca0a4f1fc5
Gary Katsevman <gary@mux.com>
2023-04-17 15:39:15 -0400

    test: use playwright to test in firefox and webkit and chrome with 'test --all'

--- test/web-test-runner.config.js
+++ test/web-test-runner.config.js
@@ -1,6 +1,6 @@
-// import { playwrightLauncher } from '@web/test-runner-playwright';
+import { playwrightLauncher } from '@web/test-runner-playwright';
 
-export default /** @type {import("@web/test-runner").TestRunnerConfig} */ ({
+const config = {
   /** Test files to run */
   files: 'test/@(unit|integration)/**/*.@(test|spec).js',
 
@@ -10,18 +10,24 @@ export default /** @type {import("@web/test-runner").TestRunnerConfig} */ ({
   /** Compile JS for older browsers. Requires @web/dev-server-esbuild plugin */
   // esbuildTarget: 'auto',
 
-  /** Amount of browsers to run concurrently */
-  // concurrentBrowsers: 2,
-
   /** Amount of test files per browser to test concurrently */
   // concurrency: 1,
+};
 
-  /** Browsers to run tests on */
-  // browsers: [
-  //   playwrightLauncher({ product: 'chromium' }),
-  //   playwrightLauncher({ product: 'firefox' }),
-  //   playwrightLauncher({ product: 'webkit' }),
-  // ],
+if (process.argv.some((arg) => arg.includes('--all'))) {
+  Object.assign(config, {
+    concurrentBrowsers: 3,
+    browsers: [
+      playwrightLauncher({
+        product: 'chromium',
+        launchOptions: {
+          channel: 'chrome',
+        },
+      }),
+      playwrightLauncher({ product: 'firefox' }),
+      playwrightLauncher({ product: 'webkit' }),
+    ],
+  });
+}
 
-  // See documentation for all available options
-});
+export default /** @type {import("@web/test-runner").TestRunnerConfig} */ config;
