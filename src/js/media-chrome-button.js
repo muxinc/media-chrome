239dad31b1e8f924e96b5ac8bdcc83989d85683a
Wesley Luyten <me@wesleyluyten.com>
2023-04-21 11:39:49 -0500

    docs: add short description to range, display

--- src/js/media-chrome-button.js
+++ src/js/media-chrome-button.js
@@ -63,27 +63,27 @@ template.innerHTML = /*html*/`
  * @preserve
  * @extends {HTMLElement}
  *
- * @cssproperty --media-text-color - The `color` of button text.
- * @cssproperty --media-icon-color - The `fill` color of button icon.
- * @cssproperty --media-primary-color - The default color of text and icon.
- * @cssproperty --media-secondary-color - The default color of button background.
+ * @cssproperty --media-primary-color - Default color of text and icon.
+ * @cssproperty --media-secondary-color - Default color of button background.
+ * @cssproperty --media-text-color - `color` of button text.
+ * @cssproperty --media-icon-color - `fill` color of button icon.
  *
- * @cssproperty --media-control-display - The `display` property of control.
- * @cssproperty --media-control-background - The `background` of control.
- * @cssproperty --media-control-hover-background - The `background` of control hover state.
- * @cssproperty --media-control-padding - The `padding` of control.
- * @cssproperty --media-control-height - The `line-height` of control.
+ * @cssproperty --media-control-display - `display` property of control.
+ * @cssproperty --media-control-background - `background` of control.
+ * @cssproperty --media-control-hover-background - `background` of control hover state.
+ * @cssproperty --media-control-padding - `padding` of control.
+ * @cssproperty --media-control-height - `line-height` of control.
  *
- * @cssproperty --media-font - The `font` shorthand property.
- * @cssproperty --media-font-weight - The `font-weight` property.
- * @cssproperty --media-font-family - The `font-family` property.
- * @cssproperty --media-font-size - The `font-size` property.
- * @cssproperty --media-text-content-height - The `line-height` of button text.
+ * @cssproperty --media-font - `font` shorthand property.
+ * @cssproperty --media-font-weight - `font-weight` property.
+ * @cssproperty --media-font-family - `font-family` property.
+ * @cssproperty --media-font-size - `font-size` property.
+ * @cssproperty --media-text-content-height - `line-height` of button text.
  *
- * @cssproperty --media-button-icon-width - The `width` of button icon.
- * @cssproperty --media-button-icon-height - The `height` of button icon.
- * @cssproperty --media-button-icon-transform - The `transform` of button icon.
- * @cssproperty --media-button-icon-transition - The `transition` of button icon.
+ * @cssproperty --media-button-icon-width - `width` of button icon.
+ * @cssproperty --media-button-icon-height - `height` of button icon.
+ * @cssproperty --media-button-icon-transform - `transform` of button icon.
+ * @cssproperty --media-button-icon-transition - `transition` of button icon.
  */
 class MediaChromeButton extends window.HTMLElement {
   #mediaController;
