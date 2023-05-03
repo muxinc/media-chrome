fefbaacdf4be1007cb32cfe0a59e4be57d4ade16
Adam Jaggard <ajaggard@mux.com>
2023-05-03 14:55:31 +0100

    media captions button props

--- src/js/media-captions-button.js
+++ src/js/media-captions-button.js
@@ -2,7 +2,11 @@ import MediaChromeButton from './media-chrome-button.js';
 import { window, document } from './utils/server-safe-globals.js';
 import { MediaUIAttributes } from './constants.js';
 import { nouns } from './labels/labels.js';
-import { areSubsOn, toggleSubsCaps } from './utils/captions.js';
+import {
+  areSubsOn,
+  splitTextTracksStr,
+  toggleSubsCaps,
+} from './utils/captions.js';
 
 const ccIconOn = `<svg aria-hidden="true" viewBox="0 0 26 24">
   <path d="M22.83 5.68a2.58 2.58 0 0 0-2.3-2.5c-3.62-.24-11.44-.24-15.06 0a2.58 2.58 0 0 0-2.3 2.5c-.23 4.21-.23 8.43 0 12.64a2.58 2.58 0 0 0 2.3 2.5c3.62.24 11.44.24 15.06 0a2.58 2.58 0 0 0 2.3-2.5c.23-4.21.23-8.43 0-12.64Zm-11.39 9.45a3.07 3.07 0 0 1-1.91.57 3.06 3.06 0 0 1-2.34-1 3.75 3.75 0 0 1-.92-2.67 3.92 3.92 0 0 1 .92-2.77 3.18 3.18 0 0 1 2.43-1 2.94 2.94 0 0 1 2.13.78c.364.359.62.813.74 1.31l-1.43.35a1.49 1.49 0 0 0-1.51-1.17 1.61 1.61 0 0 0-1.29.58 2.79 2.79 0 0 0-.5 1.89 3 3 0 0 0 .49 1.93 1.61 1.61 0 0 0 1.27.58 1.48 1.48 0 0 0 1-.37 2.1 2.1 0 0 0 .59-1.14l1.4.44a3.23 3.23 0 0 1-1.07 1.69Zm7.22 0a3.07 3.07 0 0 1-1.91.57 3.06 3.06 0 0 1-2.34-1 3.75 3.75 0 0 1-.92-2.67 3.88 3.88 0 0 1 .93-2.77 3.14 3.14 0 0 1 2.42-1 3 3 0 0 1 2.16.82 2.8 2.8 0 0 1 .73 1.31l-1.43.35a1.49 1.49 0 0 0-1.51-1.21 1.61 1.61 0 0 0-1.29.58A2.79 2.79 0 0 0 15 12a3 3 0 0 0 .49 1.93 1.61 1.61 0 0 0 1.27.58 1.44 1.44 0 0 0 1-.37 2.1 2.1 0 0 0 .6-1.15l1.4.44a3.17 3.17 0 0 1-1.1 1.7Z"/>
@@ -13,15 +17,14 @@ const ccIconOff = `<svg aria-hidden="true" viewBox="0 0 26 24">
 </svg>`;
 
 const slotTemplate = document.createElement('template');
-slotTemplate.innerHTML = /*html*/`
+slotTemplate.innerHTML = /*html*/ `
   <style>
   :host([aria-checked="true"]) slot:not([name=on]) > *,
   :host([aria-checked="true"]) ::slotted(:not([slot=on])) {
     display: none !important;
   }
 
-  ${/* Double negative, but safer if display doesn't equal 'block' */ ''
-  }
+  ${/* Double negative, but safer if display doesn't equal 'block' */ ''}
   :host(:not([aria-checked="true"])) slot:not([name=off]) > *,
   :host(:not([aria-checked="true"])) ::slotted(:not([slot=off])) {
     display: none !important;
@@ -74,6 +77,42 @@ class MediaCaptionsButton extends MediaChromeButton {
     }
 
     super.attributeChangedCallback(attrName, oldValue, newValue);
+  }
+
+  /**
+   * @type {Array<string>} An array of string serialised text tracks
+   */
+  get mediaSubtitlesList() {
+    const attrVal = this.getAttribute(MediaUIAttributes.MEDIA_SUBTITLES_LIST);
+    return attrVal ? splitTextTracksStr(attrVal) : [];
+  }
+
+  set mediaSubtitlesList(list) {
+    const newVal = list.join(' ');
+    // avoid triggering a set if no change
+    if (newVal === this.getAttribute(MediaUIAttributes.MEDIA_SUBTITLES_LIST)) {
+      return;
+    }
+    this.setAttribute(MediaUIAttributes.MEDIA_SUBTITLES_LIST, newVal);
+  }
+
+  /**
+   * @type {string | undefined} A text track represented as a string
+   */
+  get mediaSubtitlesShowing() {
+    return (
+      this.getAttribute(MediaUIAttributes.MEDIA_SUBTITLES_SHOWING) ?? undefined
+    );
+  }
+
+  set mediaSubtitlesShowing(value) {
+    // avoid triggering a set if no change
+    if (value === this.mediaSubtitlesShowing) return;
+    if (value === undefined) {
+      this.removeAttribute(MediaUIAttributes.MEDIA_SUBTITLES_SHOWING);
+      return;
+    }
+    this.setAttribute(MediaUIAttributes.MEDIA_SUBTITLES_SHOWING, value);
   }
 
   handleClick() {
