5292783ccce7da7d113f79c6271182558dbbc811
Adam Jaggard <ajaggard@mux.com>
2023-05-05 15:12:10 +0100

    spelling

--- src/js/utils/element-utils.js
+++ src/js/utils/element-utils.js
@@ -98,7 +98,7 @@ export function setNumericAttr(el, attrName, value) {
     return;
   }
 
-  // force to a numberic value before setting as a string
+  // force to a numeric value before setting as a string
   el.setAttribute(attrName, `${+value}`);
 }
 
