8c9f0d21b8215f2f64b5b3b63ced60e6f8bd7086
Wesley Luyten <me@wesleyluyten.com>
2023-05-10 14:55:59 -0500

    docs: fix Sandpack component (#589)
    Test url:
    https://media-chrome-docs-git-fork-luwes-fix-sandpack-mux.vercel.app/en/components/media-play-button
    
    - fixes the layout shift when loading Sandpack (must set a fixed height)
    - wraps the JSX component in an Astro component because we want to style
    the container wrapping the `<astro-island>`
    - fixes the styling mismatches of the Sandpack editor code between SSR
    and client
    - fixes an issue when a fixed height is set, on mobile this should be
    set to auto again because the 2 columns would become stacked
    - adds Github light theme for Sandpack, matches better with the Github
    dark we use for the markdown code fences

---
+++ docs/src/components/Sandpack.jsx
@@ -0,0 +1,80 @@
+/** @jsxImportSource react */
+import { Sandpack } from "@codesandbox/sandpack-react";
+import { githubLight } from "@codesandbox/sandpack-themes";
+
+export const Active = {
+  HTML: 'html',
+  CSS: 'css',
+};
+
+export default function ComponentSandpack({
+  html,
+  css,
+  hiddenCss = '',
+  height = 230,
+  editorHeight,
+  files = {},
+  dependencies = {},
+  active = Active.HTML,
+  ...props
+}) {
+  const importPaths = [
+    'media-chrome',
+    './styles.css',
+    ...Object.keys(dependencies),
+    ...Object.keys(files).reduce((importPaths, fileAbsPath) => {
+      // Only automatically import .css or .js files for now
+      if (fileAbsPath.endsWith('.css') | fileAbsPath.endsWith('.js')) {
+        importPaths.push(`.${fileAbsPath}`);
+      }
+      return importPaths;
+    }, css ? ['./custom-styles.css'] : [])
+  ];
+  return (
+    <Sandpack
+      template="vanilla"
+      theme={{
+        ...githubLight,
+        font: {
+          ...githubLight.font,
+          mono: 'var(--font-mono)',
+        }
+      }}
+      options={{
+        editorHeight: 'auto',
+        editorWidthPercentage: 50,
+      }}
+      customSetup={{
+        dependencies: {
+          'media-chrome': 'canary',
+          ...dependencies
+        },
+      }}
+      files={{
+        "/index.html": {
+          active: active === Active.HTML,
+          code: `${html}`
+        },
+        '/index.js': {
+          code: importPaths.map(path => `import '${path}';`).join(' '),
+          hidden: true,
+        },
+        '/styles.css': {
+          code: `media-controller,
+video {
+width: 100%;
+aspect-ratio: 2.4;
+}
+${hiddenCss}`,
+          hidden: true,
+        },
+        ...(css ? {'/custom-styles.css': {
+          active: active === Active.CSS,
+          code: `${css}`
+        }} : {}),
+        ...files
+      }}
+      {...props}
+    />
+  )
+}
