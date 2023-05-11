de1f4f0f940be7060c725a133b6ade62360fed60
Adam Jaggard <ajaggard@mux.com>
2023-05-11 14:38:22 +0100

    fix import

--- src/js/media-pip-button.js
+++ src/js/media-pip-button.js
@@ -7,7 +7,7 @@ import {
   getStringAttr,
   setBooleanAttr,
   setStringAttr,
-} from './utils/element-utils';
+} from './utils/element-utils.js';
 
 const pipIcon = `<svg aria-hidden="true" viewBox="0 0 28 24">
   <path d="M24 3H4a1 1 0 0 0-1 1v16a1 1 0 0 0 1 1h20a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1Zm-1 16H5V5h18v14Zm-3-8h-7v5h7v-5Z"/>
