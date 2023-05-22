a26cb8cb791f15664c00f0ab6c82098d1d039ed0
Christian Pillsbury <cpillsbury@mux.com>
2023-05-22 10:24:54 -0700

    fix: Make sure we monitor slotchange on slots for media state receivers (for e.g. themes).

--- src/js/media-controller.js
+++ src/js/media-controller.js
@@ -663,6 +663,26 @@ const monitorForMediaStateReceivers = (
     });
   };
 
+  // Storing refs & handlers here so we can remove on unsubscribe
+  const slotAndChangeHandlers = [];
+  // Need to monitor slotted elements being added/removed as well (e.g. for themes usage, among others)
+  (rootNode.querySelectorAll('slot:not([slot=media])') /** @type {HTMLSlotElement} */).forEach(el => {
+    const slotEl = /** @type {HTMLSlotElement} */ (el);
+    /** @type {HTMLElement[]} */
+    let prevSlotted= [];
+    const eventHandler =  () => {
+      prevSlotted.forEach((node) =>
+        traverseForMediaStateReceivers(node, unregisterMediaStateReceiver)
+      );
+      prevSlotted = /** @type {HTMLElement[]} */ ([...slotEl.assignedElements({ flatten: true })]);
+      prevSlotted.forEach((node) =>
+        traverseForMediaStateReceivers(node, registerMediaStateReceiver)
+      );
+    };
+    slotEl.addEventListener('slotchange', eventHandler);
+    slotAndChangeHandlers.push([slotEl, eventHandler]);
+  });
+
   const observer = new MutationObserver(mutationCallback);
   observer.observe(rootNode, {
     childList: true,
@@ -673,6 +693,8 @@ const monitorForMediaStateReceivers = (
   const unsubscribe = () => {
     // Unregister any Media State Receiver descendants (including ourselves)
     traverseForMediaStateReceivers(rootNode, unregisterMediaStateReceiver);
+    // Make sure we remove all slotchange event listeners
+    slotAndChangeHandlers.forEach(([slotEl, eventHandler]) => slotEl.removeEventListener(eventHandler));
     // Stop observing for Media State Receivers
     observer.disconnect();
     // Stop listening for Media State Receiver events.
