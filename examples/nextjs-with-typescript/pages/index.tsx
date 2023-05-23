861d1a74a5890dc95f6493afadf5598f335fcc52
Christian Pillsbury <cpillsbury@mux.com>
2023-05-23 07:48:24 -0700

    chore: update next example app for general cleanup. use different port than docs for dev.

--- examples/nextjs-with-typescript/pages/index.tsx
+++ examples/nextjs-with-typescript/pages/index.tsx
@@ -47,6 +47,7 @@ const Home: NextPage = () => {
         <MediaController
           ref={(node: HTMLElement) => console.log('ref', node)}
           style={chromeStyles}
+          defaultSubtitles
         >
           <video
             slot="media"
@@ -71,23 +72,23 @@ const Home: NextPage = () => {
           <MediaPosterImage
             slot="poster"
             src="https://image.mux.com/DS00Spx1CV902MCtPj5WknGlR102V5HFkDe/thumbnail.jpg"
-            placeholdersrc="data:image/jpeg;base64,/9j/2wBDAAUDBAQEAwUEBAQFBQUGBwwIBwcHBw8LCwkMEQ8SEhEPERETFhwXExQaFRERGCEYGh0dHx8fExciJCIeJBweHx7/2wBDAQUFBQcGBw4ICA4eFBEUHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh7/wAARCAASACADASIAAhEBAxEB/8QAGgABAAIDAQAAAAAAAAAAAAAAAAMEAgUGCP/EACkQAAEDAgMIAgMAAAAAAAAAAAEAAgMEBgUREgcUITFSkZTRQaEiscH/xAAYAQACAwAAAAAAAAAAAAAAAAAABQIDBv/EAB0RAAICAQUAAAAAAAAAAAAAAAABAgMFERUxwfD/2gAMAwEAAhEDEQA/AOZh2P2k/LOhq/Lf7VuPYvZxLQ6iqgXchvrxn9rpY7ojYCBU0IJ5HU3h9rU3NcGJVcVNJh2K4fDPTztlbm5reGRDhnxIzBPwkUc9RJ6dDHaLYojj2HWYeeH1nmSe1OzYXZJ54fW+ZJ7VeWrbO4SPuedpI/IOnB/TgsxJh4yIuGYu+TvAH9UXnafItWJmuTy1oZ0t7JoZ0t7Ii0InGhnS3smhnS3siIA//9k="
+            placeholderSrc="data:image/jpeg;base64,/9j/2wBDAAUDBAQEAwUEBAQFBQUGBwwIBwcHBw8LCwkMEQ8SEhEPERETFhwXExQaFRERGCEYGh0dHx8fExciJCIeJBweHx7/2wBDAQUFBQcGBw4ICA4eFBEUHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh7/wAARCAASACADASIAAhEBAxEB/8QAGgABAAIDAQAAAAAAAAAAAAAAAAMEAgUGCP/EACkQAAEDAgMIAgMAAAAAAAAAAAEAAgMEBgUREgcUITFSkZTRQaEiscH/xAAYAQACAwAAAAAAAAAAAAAAAAAABQIDBv/EAB0RAAICAQUAAAAAAAAAAAAAAAABAgMFERUxwfD/2gAMAwEAAhEDEQA/AOZh2P2k/LOhq/Lf7VuPYvZxLQ6iqgXchvrxn9rpY7ojYCBU0IJ5HU3h9rU3NcGJVcVNJh2K4fDPTztlbm5reGRDhnxIzBPwkUc9RJ6dDHaLYojj2HWYeeH1nmSe1OzYXZJ54fW+ZJ7VeWrbO4SPuedpI/IOnB/TgsxJh4yIuGYu+TvAH9UXnafItWJmuTy1oZ0t7JoZ0t7Ii0InGhnS3smhnS3siIA//9k="
           ></MediaPosterImage>
-          <div slot="centered-chrome" no-auto-hide="">
-            <MediaLoadingIndicator
-              style={{ '--media-loading-icon-width': 80 }}
-            ></MediaLoadingIndicator>
-          </div>
+          <MediaLoadingIndicator
+            noautohide
+            slot="centered-chrome"
+            style={{ '--media-loading-indicator-icon-height': '200px' }}
+          ></MediaLoadingIndicator>
           <MediaControlBar>
             <MediaPlayButton></MediaPlayButton>
-            <MediaSeekBackwardButton></MediaSeekBackwardButton>
-            <MediaSeekForwardButton></MediaSeekForwardButton>
+            <MediaSeekBackwardButton seekOffset={10}></MediaSeekBackwardButton>
+            <MediaSeekForwardButton seekOffset={10}></MediaSeekForwardButton>
             <MediaTimeRange></MediaTimeRange>
             <MediaTimeDisplay showDuration></MediaTimeDisplay>
             <MediaMuteButton></MediaMuteButton>
             <MediaVolumeRange></MediaVolumeRange>
             <MediaPlaybackRateButton></MediaPlaybackRateButton>
-            <MediaCaptionsButton defaultShowing></MediaCaptionsButton>
+            <MediaCaptionsButton></MediaCaptionsButton>
             <MediaAirplayButton></MediaAirplayButton>
             <MediaPipButton></MediaPipButton>
             <MediaFullscreenButton></MediaFullscreenButton>
