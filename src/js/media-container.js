40be40ca215cf2cdc05fe71ae08b934ad3e038a7
Liam Potter <radioactivity@gmail.com>
2023-07-26 19:18:04 +0100

    Define the return type so it is not inferred as NodeJS.Timeout

--- src/js/media-container.js
+++ src/js/media-container.js
@@ -486,6 +486,7 @@ class MediaContainer extends globalThis.HTMLElement {
       // Setting autohide to -1 turns off autohide
       if (this.autohide < 0) return;
 
+      /** @type {ReturnType<typeof setTimeout>} */
       this._inactiveTimeout = setTimeout(() => {
         setInactive();
       }, this.autohide * 1000);
