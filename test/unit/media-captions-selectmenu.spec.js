c1138134c3eeeaf2cbd46c91ebc17e001f8c8d99
Wesley Luyten <me@wesleyluyten.com>
2023-08-10 20:35:08 -0500

    chore: add more selectmenu tests

---
+++ test/unit/media-captions-selectmenu.spec.js
@@ -0,0 +1,46 @@
+import { assert, fixture } from '@open-wc/testing';
+import '../../src/js/index.js';
+import '../../src/js/experimental/index.js';
+
+describe('<media-captions-selectmenu>', () => {
+  let mediaController;
+  let selectmenu;
+  let listbox;
+
+  beforeEach(async () => {
+    mediaController = await fixture(/*html*/`
+      <media-controller>
+        <video
+          slot="media"
+          preload="auto"
+          src="https://d2zihajmogu5jn.cloudfront.net/elephantsdream/ed_hd.mp4"
+          muted
+          crossorigin
+        >
+          <track label="English" kind="captions" srclang="en" src="../../examples/vanilla/vtt/elephantsdream/captions.en.vtt" default></track>
+          <track label="Japanese" kind="captions" srclang="ja" src="../../examples/vanilla/vtt/elephantsdream/captions.ja.vtt"></track>
+          <track label="Swedish" kind="captions" srclang="sv" src="../../examples/vanilla/vtt/elephantsdream/captions.sv.vtt"></track>
+          <track label="Russian" kind="captions" srclang="ru" src="../../examples/vanilla/vtt/elephantsdream/captions.ru.vtt"></track>
+          <track label="Arabic" kind="captions" srclang="ar" src="../../examples/vanilla/vtt/elephantsdream/captions.ar.vtt"></track>
+          <track label="Subs English" kind="subtitles" srclang="en" src="../../examples/vanilla/vtt/elephantsdream/captions.en.vtt" default></track>
+          <track label="Subs Japanese" kind="subtitles" srclang="ja" src="../../examples/vanilla/vtt/elephantsdream/captions.ja.vtt"></track>
+          <track label="Subs Swedish" kind="subtitles" srclang="sv" src="../../examples/vanilla/vtt/elephantsdream/captions.sv.vtt"></track>
+          <track label="Subs Russian" kind="subtitles" srclang="ru" src="../../examples/vanilla/vtt/elephantsdream/captions.ru.vtt"></track>
+          <track label="Subs Arabic" kind="subtitles" srclang="ar" src="../../examples/vanilla/vtt/elephantsdream/captions.ar.vtt"></track>
+        </video>
+        <media-captions-selectmenu></media-captions-selectmenu>
+      </media-controller>
+    `);
+    selectmenu = mediaController.querySelector('media-captions-selectmenu');
+    listbox = selectmenu.shadowRoot.querySelector('media-captions-listbox');
+  });
+
+  it('selectmenu is populated', async function () {
+    await new Promise(resolve => mediaController.media.textTracks.addEventListener('addtrack', resolve));
+
+    assert.equal(mediaController.media.textTracks.length, 10);
+    assert.equal(listbox.options.length, 11); // includes Off option
+    // assert.equal(listbox.value, 'cc:en:English'); fails on Firefox and Safari
+  });
+
+});
--- test/unit/media-playback-rate-selectmenu.spec.js
+++ test/unit/media-playback-rate-selectmenu.spec.js
@@ -2,8 +2,9 @@ import { assert, fixture } from '@open-wc/testing';
 import '../../src/js/index.js';
 import '../../src/js/experimental/index.js';
 
-describe('<media-playback-rate-listbox>', () => {
+describe('<media-playback-rate-selectmenu>', () => {
   let mediaController;
+  let selectmenu;
   let listbox;
 
   beforeEach(async () => {
@@ -14,13 +15,14 @@ describe('<media-playback-rate-listbox>', () => {
           src="https://stream.mux.com/A3VXy02VoUinw01pwyomEO3bHnG4P32xzV7u1j1FSzjNg/low.mp4"
           muted
         ></video>
-        <media-playback-rate-listbox></media-playback-rate-listbox>
+        <media-playback-rate-selectmenu></media-playback-rate-selectmenu>
       </media-controller>
     `);
-    listbox = mediaController.querySelector('media-playback-rate-listbox');
+    selectmenu = mediaController.querySelector('media-playback-rate-selectmenu');
+    listbox = selectmenu.shadowRoot.querySelector('media-playback-rate-listbox');
   });
 
-  it('listbox is populated', async function () {
+  it('selectmenu is populated', async function () {
     assert.equal(listbox.options.length, 5);
     assert.equal(listbox.value, 1);
   });
