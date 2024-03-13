a96e6bd6055da1701f18bb0c8cc4a2183b221d2b
Christian Pillsbury <cpillsbury@mux.com>
2024-03-13 07:54:26 -0700

    feat(react-hooks): update builds, src location, and dependencies.

--- src/js/react/media-store.tsx
+++ src/js/react/media-store.tsx
@@ -1,17 +1,14 @@
 import { createContext, useContext, useEffect, useMemo } from 'react';
 import type { Context, ReactNode } from 'react';
 import React from 'react';
-// NOTE: This is an official package published by the React team and part of their monorepo: https://github.com/facebook/react/tree/main/packages/use-sync-external-store
-// We may be able to migrate to https://react.dev/reference/react/useSyncExternalStore instead if preferred
-import { useSyncExternalStoreWithSelector } from 'use-sync-external-store/with-selector';
-// import { useSyncExternalStore } from 'react';
-import createMediaStore from '../js/media-store/media-store';
+import { useSyncExternalStoreWithSelector } from './useSyncExternalStoreWithSelector';
+import createMediaStore from '../media-store/media-store';
 import type {
   FullScreenElementStateOwner,
   MediaState,
   MediaStateOwner,
   MediaStore,
-} from '../js/media-store/media-store';
+} from '../media-store/media-store';
 
 // The context is the thing that allows for a shared... context that propagates outside of the (V)DOM/Component tree.
 // Contexts have values. Our value will be the the media ui store (aka the "guts" of Media Controller state mgmt)
@@ -33,9 +30,15 @@ export const Provider = ({
     [mediaStore]
   );
   useEffect(() => {
-    value?.dispatch({ type: 'documentelementchangerequest', detail: globalThis.document });
+    value?.dispatch({
+      type: 'documentelementchangerequest',
+      detail: globalThis.document,
+    });
     return () => {
-      value?.dispatch({ type: 'documentelementchangerequest', detail: undefined });
+      value?.dispatch({
+        type: 'documentelementchangerequest',
+        detail: undefined,
+      });
     };
   }, []);
   return (
@@ -62,32 +65,6 @@ export const useMediaDispatch = () => {
   }) as MediaStore['dispatch'];
 };
 
-const refEquality = (a: any, b: any) => a === b;
-
-// This is a hook to get access to the media state. It accepts a function that let's you grab
-// only the bit of state you care about to avoid unnecessary re-renders in react. It also
-// allows you to pass in a more complex equality check (since you can transform the state
-// or might only care about a subset of state changes, say only caring about second precision for time updates).
-// This is a familiar convention in react (+ hooks) state mgmt world. It also grabs the context
-// (again, with the "inherited" value provided from the ancestor Provider) and both subscribes to
-// the state and pushes out updates whenever there are actual changes to the state from the
-// selector.
-export const useMediaSelector = <S = any,>(
-  selector: (state: Partial<MediaState>) => S,
-  equalityFn = refEquality
-) => {
-  const store = useContext(MediaContext) as MediaStore;
-  const selectedState = useSyncExternalStoreWithSelector(
-    store?.subscribe ?? (() => {}),
-    store?.getState ?? (() => ({})),
-    store?.getState ?? (() => ({})),
-    selector,
-    equalityFn
-  ) as S;
-
-  return selectedState;
-};
-
 // We could make something like this official.
 // Also, "Name Are Hard TM"
 export const useMediaRefCallback = () => {
@@ -108,4 +85,32 @@ export const useFullscreenRefCallback = () => {
     // the corresponding react element instance (e.g. a `<div>`) is being removed.
     dispatch({ type: 'fullscreenelementchangerequest', detail: fullscreenEl });
   };
+};
+
+const refEquality = (a: any, b: any) => a === b;
+
+// This is a hook to get access to the media state. It accepts a function that let's you grab
+// only the bit of state you care about to avoid unnecessary re-renders in react. It also
+// allows you to pass in a more complex equality check (since you can transform the state
+// or might only care about a subset of state changes, say only caring about second precision for time updates).
+// This is a familiar convention in react (+ hooks) state mgmt world. It also grabs the context
+// (again, with the "inherited" value provided from the ancestor Provider) and both subscribes to
+// the state and pushes out updates whenever there are actual changes to the state from the
+// selector.
+// NOTE: Moved this to the bottom bc JSX/TSX <Component> and TS <Generic> get confused by syntax highlighting (and there
+// are even casses where the compiler may not be able to successfully disambiguate the two) (CJP)
+export const useMediaSelector = <S = any>(
+  selector: (state: Partial<MediaState>) => S,
+  equalityFn = refEquality
+) => {
+  const store = useContext(MediaContext) as MediaStore;
+  const selectedState = useSyncExternalStoreWithSelector(
+    store?.subscribe ?? (() => () => {}),
+    store?.getState ?? (() => ({})),
+    store?.getState ?? (() => ({})),
+    selector,
+    equalityFn
+  ) as S;
+
+  return selectedState;
 };
---
+++ src/js/react/useSyncExternalStoreWithSelector.ts
@@ -0,0 +1,143 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import {
+  useRef,
+  useEffect,
+  useMemo,
+  useDebugValue,
+  useSyncExternalStore,
+} from 'react';
+
+// NOTE: This is a port of https://github.com/facebook/react/blob/main/packages/use-sync-external-store/src/useSyncExternalStoreWithSelector.js
+// and React's internal/shared version of `Object.is` with refactors for TypeScript and bundling. Doing this to avoid adding an additional dependency
+// beyond core React when using the `MediaStore` React hooks and related (CJP)
+
+/**
+ * inlined Object.is polyfill to avoid requiring consumers ship their own
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/is
+ */
+function isPolyfill(x: any, y: any) {
+  return (
+    (x === y && (x !== 0 || 1 / x === 1 / y)) || (x !== x && y !== y) // eslint-disable-line no-self-compare
+  );
+}
+
+const is: (x: any, y: any) => boolean =
+  typeof Object.is === 'function' ? Object.is : isPolyfill;
+
+// Same as useSyncExternalStore, but supports selector and isEqual arguments.
+export function useSyncExternalStoreWithSelector<Snapshot, Selection>(
+  subscribe: (onStoreChange: () => void) => () => void,
+  getSnapshot: () => Snapshot,
+  getServerSnapshot: undefined | null | (() => Snapshot),
+  selector: (snapshot: Snapshot) => Selection,
+  isEqual?: (a: Selection, b: Selection) => boolean
+) {
+  // Use this to track the rendered snapshot.
+  const instRef = useRef<
+    | {
+        hasValue: true;
+        value: Selection;
+      }
+    | {
+        hasValue: false;
+        value: null;
+      }
+    | null
+  >(null);
+  let inst;
+  if (instRef.current === null) {
+    inst = {
+      hasValue: false,
+      value: null,
+    };
+    instRef.current = inst;
+  } else {
+    inst = instRef.current;
+  }
+
+  const [getSelection, getServerSelection] = useMemo(() => {
+    // Track the memoized state using closure variables that are local to this
+    // memoized instance of a getSnapshot function. Intentionally not using a
+    // useRef hook, because that state would be shared across all concurrent
+    // copies of the hook/component.
+    let hasMemo = false;
+    let memoizedSnapshot;
+    let memoizedSelection: Selection;
+    const memoizedSelector = (nextSnapshot: Snapshot) => {
+      if (!hasMemo) {
+        // The first time the hook is called, there is no memoized result.
+        hasMemo = true;
+        memoizedSnapshot = nextSnapshot;
+        const nextSelection = selector(nextSnapshot);
+        if (isEqual !== undefined) {
+          // Even if the selector has changed, the currently rendered selection
+          // may be equal to the new selection. We should attempt to reuse the
+          // current value if possible, to preserve downstream memoizations.
+          if (inst.hasValue) {
+            const currentSelection = inst.value;
+            if (isEqual(currentSelection, nextSelection)) {
+              memoizedSelection = currentSelection;
+              return currentSelection;
+            }
+          }
+        }
+        memoizedSelection = nextSelection;
+        return nextSelection;
+      }
+
+      // We may be able to reuse the previous invocation's result.
+      const prevSnapshot: Snapshot = memoizedSnapshot;
+      const prevSelection: Selection = memoizedSelection;
+
+      if (is(prevSnapshot, nextSnapshot)) {
+        // The snapshot is the same as last time. Reuse the previous selection.
+        return prevSelection;
+      }
+
+      // The snapshot has changed, so we need to compute a new selection.
+      const nextSelection = selector(nextSnapshot);
+
+      // If a custom isEqual function is provided, use that to check if the data
+      // has changed. If it hasn't, return the previous selection. That signals
+      // to React that the selections are conceptually equal, and we can bail
+      // out of rendering.
+      if (isEqual !== undefined && isEqual(prevSelection, nextSelection)) {
+        return prevSelection;
+      }
+
+      memoizedSnapshot = nextSnapshot;
+      memoizedSelection = nextSelection;
+      return nextSelection;
+    };
+    // Assigning this to a constant so that Flow knows it can't change.
+    const maybeGetServerSnapshot =
+      getServerSnapshot === undefined ? null : getServerSnapshot;
+    const getSnapshotWithSelector = () => memoizedSelector(getSnapshot());
+    const getServerSnapshotWithSelector =
+      maybeGetServerSnapshot === null
+        ? undefined
+        : () => memoizedSelector(maybeGetServerSnapshot());
+    return [getSnapshotWithSelector, getServerSnapshotWithSelector];
+  }, [getSnapshot, getServerSnapshot, selector, isEqual]);
+
+  const value = useSyncExternalStore(
+    subscribe,
+    getSelection,
+    getServerSelection
+  );
+
+  useEffect(() => {
+    inst.hasValue = true;
+    inst.value = value;
+  }, [value]);
+
+  useDebugValue(value);
+  return value;
+}
