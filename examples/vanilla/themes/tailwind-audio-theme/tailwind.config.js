a571e5fab2814849e25d7ae84758637ca5459cb6
Wesley Luyten <me@wesleyluyten.com>
2024-03-22 17:06:51 -0500

    example: add tailwind css audio theme (#838)
    https://media-chrome-git-fork-luwes-tailwind-audio-theme-mux.vercel.app/examples/vanilla/themes/tailwind-audio-theme/

---
+++ examples/vanilla/themes/tailwind-audio-theme/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/container-queries'),
+  ],
+}
