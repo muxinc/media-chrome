6db1e222a5da794afbb603eeae7499d34361d394
Adam Jaggard <ajaggard@mux.com>
2024-07-19 11:59:44 +0100

    fix positional arrows

--- src/js/media-tooltip.ts
+++ src/js/media-tooltip.ts
@@ -27,7 +27,7 @@ template.innerHTML = /*html*/ `
       justify-content: center;
       align-items: center;
       box-sizing: border-box;
-      background: #fff;
+      background: var(--media-tooltip-background, #fff);
       color: #000;
       font-weight: 400;
       font-family: var(--media-font-family, helvetica neue, segoe ui, roboto, arial, sans-serif);
@@ -46,15 +46,10 @@ template.innerHTML = /*html*/ `
 
     #arrow {
       position: absolute;
-      top: 100%;
-      left: 50%;
       width: 0px;
       height: 0px;
       border-style: solid;
-      border-width: 5px 6px 0 6px;
-      border-color: #fff transparent transparent transparent;
-      transform: rotate(0deg);
-      transform: translate(calc(-50% + var(--media-tooltip-offset-x, 0px)), 0);
+      display: var(--media-tooltip-arrow-display, block);
     }
 
     :host(:not([position])),
@@ -64,28 +59,60 @@ template.innerHTML = /*html*/ `
       left: 50%;
       transform: translate(calc(-50% - var(--media-tooltip-offset-x, 0px)), 0);
     }
+    :host(:not([position])) #arrow,
+    :host([position="top"]) #arrow {
+      top: 100%;
+      left: 50%;
+      border-width: 5px 6px 0 6px;
+      border-color: var(--media-tooltip-background, #fff) transparent transparent transparent;
+      transform: translate(calc(-50% + var(--media-tooltip-offset-x, 0px)), 0);
+    }
+
     :host([position="right"]) {
       position: absolute;
       left: calc(100% + var(--media-tooltip-distance, 12px));
       top: 50%;
       transform: translate(0, -50%);
     }
+    :host([position="right"]) #arrow {
+      top: 50%;
+      right: 100%;
+      border-width: 6px 5px 6px 0;
+      border-color: transparent var(--media-tooltip-background, #fff) transparent transparent;
+      transform: translate(0, -50%);
+    }
+
     :host([position="bottom"]) {
       position: absolute;
       top: calc(100% + var(--media-tooltip-distance, 12px));
       left: 50%;
       transform: translate(-50%, 0);
     }
+    :host([position="bottom"]) #arrow {
+      bottom: 100%;
+      left: 50%;
+      border-width: 0 6px 5px 6px;
+      border-color: transparent transparent var(--media-tooltip-background, #fff) transparent;
+      transform: translate(calc(-50% + var(--media-tooltip-offset-x, 0px)), 0);
+    }
+
     :host([position="left"]) {
       position: absolute;
       right: calc(100% + var(--media-tooltip-distance, 12px));
       top: 50%;
       transform: translate(0, -50%);
     }
-    /*
-    * Because we default to "top" style if no attr is set we can
-    /* let it fall back to default style for "none"
-    /* :host([position="none"]) .container {} */
+    :host([position="left"]) #arrow {
+      top: 50%;
+      left: 100%;
+      border-width: 6px 0 6px 5px;
+      border-color: transparent transparent transparent var(--media-tooltip-background, #fff);
+      transform: translate(0, -50%);
+    }
+    
+    :host([position="none"]) #arrow {
+      display: none;
+    }
 
   </style>
   <slot></slot>
