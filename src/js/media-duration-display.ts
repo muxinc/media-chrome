e3e4e922bbfedce16ddc834f50b90f97bb2fa475
Wesley Luyten <me@wesleyluyten.com>
2025-05-16 11:33:42 -0500

    fix: use props instead of attrs

--- src/js/media-duration-display.ts
+++ src/js/media-duration-display.ts
@@ -5,15 +5,9 @@ import { MediaUIAttributes } from './constants.js';
 import { getNumericAttr, setNumericAttr } from './utils/element-utils.js';
 // Todo: Use data locals: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleTimeString
 
-/**
- * Note the use of attributes here instead of props. If the prop reflects to an attribute,
- * the attribute will be preferred over the prop because that is HTML serializable on the server.
- * The 2nd arg props will only be used for complex values that are not reflected to attributes.
- * https://github.com/muxinc/ce-la-react/blob/main/src/ce-la-react.ts#L212-L223
- */
-function getSlotTemplateHTML(attrs: Record<string, string>, _props: Record<string, any>) {
+function getSlotTemplateHTML(_attrs: Record<string, string>, props: Record<string, any>) {
   return /*html*/ `
-    <slot>${formatTime(+attrs.mediaduration)}</slot>
+    <slot>${formatTime(props.mediaDuration)}</slot>
   `;
 }
 
--- src/js/media-time-display.ts
+++ src/js/media-time-display.ts
@@ -80,20 +80,7 @@ const updateAriaValueText = (el: MediaTimeDisplay): void => {
   el.setAttribute('aria-valuetext', fullPhrase);
 };
 
-/**
- * Note the use of attributes here instead of props. If the prop reflects to an attribute,
- * the attribute will be preferred over the prop because that is HTML serializable on the server.
- * The 2nd arg props will only be used for complex values that are not reflected to attributes.
- * https://github.com/muxinc/ce-la-react/blob/main/src/ce-la-react.ts#L212-L223
- */
-function getSlotTemplateHTML(attrs: Record<string, string>, _props: Record<string, any>) {
-  const props = {
-    remaining: attrs.remaining !== null,
-    showDuration: attrs.showduration !== null,
-    mediaCurrentTime: +attrs.mediacurrenttime,
-    mediaDuration: +attrs.mediaduration,
-    mediaSeekable: attrs.mediaseekable?.split(':').map((time) => +time) as [number, number],
-  }
+function getSlotTemplateHTML(_attrs: Record<string, string>, props: Record<string, any>) {
   return /*html*/ `
     <slot>${formatTimesLabel(props as MediaTimeDisplay)}</slot>
   `;
