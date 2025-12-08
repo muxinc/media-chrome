518ce3c2b9cb933d845ad917aec26123f880b612
ignasio-mux <iroure@mux.com>
2025-12-08 11:28:10 -0300

    unwanted change

--- examples/nextjs-with-typescript/next-env.d.ts
+++ examples/nextjs-with-typescript/next-env.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
-/// <reference path="./.next/types/routes.d.ts" />
+/// <reference types="next/navigation-types/compat/navigation" />
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
