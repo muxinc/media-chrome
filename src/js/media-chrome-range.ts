muxinc/media-chrome

commit 535ff62ce724368a3b85a1c7976ba9502de72ab1

feat: use getTemplateHTML (React SSR support) (#1142)
Implementation of
https://github.com/muxinc/media-chrome/discussions/1127
fix #1143 
Test page:
https://media-chrome-demo-nextjs-git-gettemplatehtml-mux.vercel.app/react-wrappers

This change introduces a new static method called
`getTemplateHTML(attrs, props)` on all the Media Chrome elements.
The pattern it replaces is roughly:

```js
const template = document.createElement('template');
template.innerHTML = `<style>...</style><slot></slot>`;
...
this.shadowRoot.append(template.content.cloneNode(true));
```

with

```js
static getTemplateHTML(attrs, props) {
	return `<style>...</style><slot></slot>`;
}
...
const attrs = namedNodeMapToObject(this.attributes);
this.shadowRoot.innerHTML = this.constructor.getTemplateHTML(attrs, {});
```

This change makes it possible to easily create a declarative shadow dom
(DSD) on the server with just the classes of the custom elements. See
https://github.com/muxinc/ce-la-react/blob/main/src/ce-la-react.ts#L305-L323

The result is that our React wrappers can now server side render the
internal HTML of Media Chrome's elements speeding up rendering of the
controls.

Looking at the code above you might have noticed the template is not
cloned any longer. That is a small performance hit in JS / DOM
processing if the app were to re-create the controls over and over but
I'm quite sure in real world it is negligible. And we could easily add
template caching if needed.

src/js/media-chrome-range.ts
@@ -3,247 +3,249 @@ import { globalThis, document } from './utils/server-safe-globals.js';
 import {
   getOrInsertCSSRule,
   getPointProgressOnLine,
+  namedNodeMapToObject,
 } from './utils/element-utils.js';
 import { observeResize, unobserveResize } from './utils/resize-observer.js';
 
-const template: HTMLTemplateElement = document.createElement('template');
-template.innerHTML = /*html*/ `
-  <style>
-    :host {
-      --_focus-box-shadow: var(--media-focus-box-shadow, inset 0 0 0 2px rgb(27 127 204 / .9));
-      --_media-range-padding: var(--media-range-padding, var(--media-control-padding, 10px));
+function getTemplateHTML(_attrs: Record<string, string>) {
+  return /*html*/ `
+    <style>
+      :host {
+        --_focus-box-shadow: var(--media-focus-box-shadow, inset 0 0 0 2px rgb(27 127 204 / .9));
+        --_media-range-padding: var(--media-range-padding, var(--media-control-padding, 10px));
 
-      box-shadow: var(--_focus-visible-box-shadow, none);
-      background: var(--media-control-background, var(--media-secondary-color, rgb(20 20 30 / .7)));
-      height: calc(var(--media-control-height, 24px) + 2 * var(--_media-range-padding));
-      display: inline-flex;
-      align-items: center;
-      ${
-        /* Don't horizontal align w/ justify-content! #container can go negative on the x-axis w/ small width. */ ''
+        box-shadow: var(--_focus-visible-box-shadow, none);
+        background: var(--media-control-background, var(--media-secondary-color, rgb(20 20 30 / .7)));
+        height: calc(var(--media-control-height, 24px) + 2 * var(--_media-range-padding));
+        display: inline-flex;
+        align-items: center;
+        ${
+          /* Don't horizontal align w/ justify-content! #container can go negative on the x-axis w/ small width. */ ''
+        }
+        vertical-align: middle;
+        box-sizing: border-box;
+        position: relative;
+        width: 100px;
+        transition: background .15s linear;
+        cursor: var(--media-cursor, pointer);
+        pointer-events: auto;
+        touch-action: none; ${/* Prevent scrolling when dragging on mobile. */ ''}
       }
-      vertical-align: middle;
-      box-sizing: border-box;
-      position: relative;
-      width: 100px;
-      transition: background .15s linear;
-      cursor: var(--media-cursor, pointer);
-      pointer-events: auto;
-      touch-action: none; ${/* Prevent scrolling when dragging on mobile. */ ''}
-    }
 
-    ${/* Reset before `outline` on track could be set by a CSS var */ ''}
-    input[type=range]:focus {
-      outline: 0;
-    }
-    input[type=range]:focus::-webkit-slider-runnable-track {
-      outline: 0;
-    }
-
-    :host(:hover) {
-      background: var(--media-control-hover-background, rgb(50 50 70 / .7));
-    }
-
-    #leftgap {
-      padding-left: var(--media-range-padding-left, var(--_media-range-padding));
-    }
-
-    #rightgap {
-      padding-right: var(--media-range-padding-right, var(--_media-range-padding));
-    }
-
-    #startpoint,
-    #endpoint {
-      position: absolute;
-    }
-
-    #endpoint {
-      right: 0;
-    }
-
-    #container {
-      ${
-        /* Not using the CSS `padding` prop makes it easier for slide open volume ranges so the width can be zero. */ ''
+      ${/* Reset before `outline` on track could be set by a CSS var */ ''}
+      input[type=range]:focus {
+        outline: 0;
       }
-      width: var(--media-range-track-width, 100%);
-      transform: translate(var(--media-range-track-translate-x, 0px), var(--media-range-track-translate-y, 0px));
-      position: relative;
-      height: 100%;
-      display: flex;
-      align-items: center;
-      min-width: 40px;
-    }
-
-    #range {
-      ${/* The input range acts as a hover and hit zone for input events. */ ''}
-      display: var(--media-time-range-hover-display, block);
-      bottom: var(--media-time-range-hover-bottom, -7px);
-      height: var(--media-time-range-hover-height, max(100% + 7px, 25px));
-      width: 100%;
-      position: absolute;
-      cursor: var(--media-cursor, pointer);
-
-      -webkit-appearance: none; ${
-        /* Hides the slider so that custom slider can be made */ ''
+      input[type=range]:focus::-webkit-slider-runnable-track {
+        outline: 0;
       }
-      -webkit-tap-highlight-color: transparent;
-      background: transparent; ${/* Otherwise white in Chrome */ ''}
-      margin: 0;
-      z-index: 1;
-    }
 
-    @media (hover: hover) {
+      :host(:hover) {
+        background: var(--media-control-hover-background, rgb(50 50 70 / .7));
+      }
+
+      #leftgap {
+        padding-left: var(--media-range-padding-left, var(--_media-range-padding));
+      }
+
+      #rightgap {
+        padding-right: var(--media-range-padding-right, var(--_media-range-padding));
+      }
+
+      #startpoint,
+      #endpoint {
+        position: absolute;
+      }
+
+      #endpoint {
+        right: 0;
+      }
+
+      #container {
+        ${
+          /* Not using the CSS `padding` prop makes it easier for slide open volume ranges so the width can be zero. */ ''
+        }
+        width: var(--media-range-track-width, 100%);
+        transform: translate(var(--media-range-track-translate-x, 0px), var(--media-range-track-translate-y, 0px));
+        position: relative;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        min-width: 40px;
+      }
+
       #range {
-        bottom: var(--media-time-range-hover-bottom, -5px);
-        height: var(--media-time-range-hover-height, max(100% + 5px, 20px));
+        ${/* The input range acts as a hover and hit zone for input events. */ ''}
+        display: var(--media-time-range-hover-display, block);
+        bottom: var(--media-time-range-hover-bottom, -7px);
+        height: var(--media-time-range-hover-height, max(100% + 7px, 25px));
+        width: 100%;
+        position: absolute;
+        cursor: var(--media-cursor, pointer);
+
+        -webkit-appearance: none; ${
+          /* Hides the slider so that custom slider can be made */ ''
+        }
+        -webkit-tap-highlight-color: transparent;
+        background: transparent; ${/* Otherwise white in Chrome */ ''}
+        margin: 0;
+        z-index: 1;
       }
-    }
 
-    ${/* Special styling for WebKit/Blink */ ''}
-    ${
-      /* Make thumb width/height small so it has no effect on range click position. */ ''
-    }
-    #range::-webkit-slider-thumb {
-      -webkit-appearance: none;
-      background: transparent;
-      width: .1px;
-      height: .1px;
-    }
-
-    ${/* The thumb is not positioned relative to the track in Firefox */ ''}
-    #range::-moz-range-thumb {
-      background: transparent;
-      border: transparent;
-      width: .1px;
-      height: .1px;
-    }
-
-    #appearance {
-      height: var(--media-range-track-height, 4px);
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      width: 100%;
-      position: absolute;
-      ${/* Required for Safari to stop glitching track height on hover */ ''}
-      will-change: transform;
-    }
-
-    #track {
-      background: var(--media-range-track-background, rgb(255 255 255 / .2));
-      border-radius: var(--media-range-track-border-radius, 1px);
-      border: var(--media-range-track-border, none);
-      outline: var(--media-range-track-outline);
-      outline-offset: var(--media-range-track-outline-offset);
-      backdrop-filter: var(--media-range-track-backdrop-filter);
-      -webkit-backdrop-filter: var(--media-range-track-backdrop-filter);
-      box-shadow: var(--media-range-track-box-shadow, none);
-      position: absolute;
-      width: 100%;
-      height: 100%;
-      overflow: hidden;
-    }
-
-    #progress,
-    #pointer {
-      position: absolute;
-      height: 100%;
-      will-change: width;
-    }
-
-    #progress {
-      background: var(--media-range-bar-color, var(--media-primary-color, rgb(238 238 238)));
-      transition: var(--media-range-track-transition);
-    }
-
-    #pointer {
-      background: var(--media-range-track-pointer-background);
-      border-right: var(--media-range-track-pointer-border-right);
-      transition: visibility .25s, opacity .25s;
-      visibility: hidden;
-      opacity: 0;
-    }
-
-    @media (hover: hover) {
-      :host(:hover) #pointer {
-        transition: visibility .5s, opacity .5s;
-        visibility: visible;
-        opacity: 1;
+      @media (hover: hover) {
+        #range {
+          bottom: var(--media-time-range-hover-bottom, -5px);
+          height: var(--media-time-range-hover-height, max(100% + 5px, 20px));
+        }
       }
-    }
 
-    #thumb,
-    ::slotted([slot=thumb]) {
-      width: var(--media-range-thumb-width, 10px);
-      height: var(--media-range-thumb-height, 10px);
-      transition: var(--media-range-thumb-transition);
-      transform: var(--media-range-thumb-transform, none);
-      opacity: var(--media-range-thumb-opacity, 1);
-      translate: -50%;
-      position: absolute;
-      left: 0;
-      cursor: var(--media-cursor, pointer);
-    }
+      ${/* Special styling for WebKit/Blink */ ''}
+      ${
+        /* Make thumb width/height small so it has no effect on range click position. */ ''
+      }
+      #range::-webkit-slider-thumb {
+        -webkit-appearance: none;
+        background: transparent;
+        width: .1px;
+        height: .1px;
+      }
 
-    #thumb {
-      border-radius: var(--media-range-thumb-border-radius, 10px);
-      background: var(--media-range-thumb-background, var(--media-primary-color, rgb(238 238 238)));
-      box-shadow: var(--media-range-thumb-box-shadow, 1px 1px 1px transparent);
-      border: var(--media-range-thumb-border, none);
-    }
+      ${/* The thumb is not positioned relative to the track in Firefox */ ''}
+      #range::-moz-range-thumb {
+        background: transparent;
+        border: transparent;
+        width: .1px;
+        height: .1px;
+      }
 
-    :host([disabled]) #thumb {
-      background-color: #777;
-    }
+      #appearance {
+        height: var(--media-range-track-height, 4px);
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        width: 100%;
+        position: absolute;
+        ${/* Required for Safari to stop glitching track height on hover */ ''}
+        will-change: transform;
+      }
 
-    .segments #appearance {
-      height: var(--media-range-segment-hover-height, 7px);
-    }
+      #track {
+        background: var(--media-range-track-background, rgb(255 255 255 / .2));
+        border-radius: var(--media-range-track-border-radius, 1px);
+        border: var(--media-range-track-border, none);
+        outline: var(--media-range-track-outline);
+        outline-offset: var(--media-range-track-outline-offset);
+        backdrop-filter: var(--media-range-track-backdrop-filter);
+        -webkit-backdrop-filter: var(--media-range-track-backdrop-filter);
+        box-shadow: var(--media-range-track-box-shadow, none);
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+      }
 
-    #track {
-      clip-path: url(#segments-clipping);
-    }
+      #progress,
+      #pointer {
+        position: absolute;
+        height: 100%;
+        will-change: width;
+      }
 
-    #segments {
-      --segments-gap: var(--media-range-segments-gap, 2px);
-      position: absolute;
-      width: 100%;
-      height: 100%;
-    }
+      #progress {
+        background: var(--media-range-bar-color, var(--media-primary-color, rgb(238 238 238)));
+        transition: var(--media-range-track-transition);
+      }
 
-    #segments-clipping {
-      transform: translateX(calc(var(--segments-gap) / 2));
-    }
+      #pointer {
+        background: var(--media-range-track-pointer-background);
+        border-right: var(--media-range-track-pointer-border-right);
+        transition: visibility .25s, opacity .25s;
+        visibility: hidden;
+        opacity: 0;
+      }
 
-    #segments-clipping:empty {
-      display: none;
-    }
+      @media (hover: hover) {
+        :host(:hover) #pointer {
+          transition: visibility .5s, opacity .5s;
+          visibility: visible;
+          opacity: 1;
+        }
+      }
 
-    #segments-clipping rect {
-      height: var(--media-range-track-height, 4px);
-      y: calc((var(--media-range-segment-hover-height, 7px) - var(--media-range-track-height, 4px)) / 2);
-      transition: var(--media-range-segment-transition, transform .1s ease-in-out);
-      transform: var(--media-range-segment-transform, scaleY(1));
-      transform-origin: center;
-    }
-  </style>
-  <div id="leftgap"></div>
-  <div id="container">
-    <div id="startpoint"></div>
-    <div id="endpoint"></div>
-    <div id="appearance">
-      <div id="track" part="track">
-        <div id="pointer"></div>
-        <div id="progress" part="progress"></div>
+      #thumb,
+      ::slotted([slot=thumb]) {
+        width: var(--media-range-thumb-width, 10px);
+        height: var(--media-range-thumb-height, 10px);
+        transition: var(--media-range-thumb-transition);
+        transform: var(--media-range-thumb-transform, none);
+        opacity: var(--media-range-thumb-opacity, 1);
+        translate: -50%;
+        position: absolute;
+        left: 0;
+        cursor: var(--media-cursor, pointer);
+      }
+
+      #thumb {
+        border-radius: var(--media-range-thumb-border-radius, 10px);
+        background: var(--media-range-thumb-background, var(--media-primary-color, rgb(238 238 238)));
+        box-shadow: var(--media-range-thumb-box-shadow, 1px 1px 1px transparent);
+        border: var(--media-range-thumb-border, none);
+      }
+
+      :host([disabled]) #thumb {
+        background-color: #777;
+      }
+
+      .segments #appearance {
+        height: var(--media-range-segment-hover-height, 7px);
+      }
+
+      #track {
+        clip-path: url(#segments-clipping);
+      }
+
+      #segments {
+        --segments-gap: var(--media-range-segments-gap, 2px);
+        position: absolute;
+        width: 100%;
+        height: 100%;
+      }
+
+      #segments-clipping {
+        transform: translateX(calc(var(--segments-gap) / 2));
+      }
+
+      #segments-clipping:empty {
+        display: none;
+      }
+
+      #segments-clipping rect {
+        height: var(--media-range-track-height, 4px);
+        y: calc((var(--media-range-segment-hover-height, 7px) - var(--media-range-track-height, 4px)) / 2);
+        transition: var(--media-range-segment-transition, transform .1s ease-in-out);
+        transform: var(--media-range-segment-transform, scaleY(1));
+        transform-origin: center;
+      }
+    </style>
+    <div id="leftgap"></div>
+    <div id="container">
+      <div id="startpoint"></div>
+      <div id="endpoint"></div>
+      <div id="appearance">
+        <div id="track" part="track">
+          <div id="pointer"></div>
+          <div id="progress" part="progress"></div>
+        </div>
+        <slot name="thumb">
+          <div id="thumb" part="thumb"></div>
+        </slot>
+        <svg id="segments"><clipPath id="segments-clipping"></clipPath></svg>
       </div>
-      <slot name="thumb">
-        <div id="thumb" part="thumb"></div>
-      </slot>
-      <svg id="segments"><clipPath id="segments-clipping"></clipPath></svg>
+      <input id="range" type="range" min="0" max="1" step="any" value="0">
     </div>
-    <input id="range" type="range" min="0" max="1" step="any" value="0">
-  </div>
-  <div id="rightgap"></div>
-`;
+    <div id="rightgap"></div>
+  `;
+}
 
 /**
  * @extends {HTMLElement}
@@ -302,6 +304,9 @@ template.innerHTML = /*html*/ `
  * @cssproperty --media-range-track-pointer-border-right - `border-right` of range track pointer.
  */
 class MediaChromeRange extends globalThis.HTMLElement {
+  static shadowRootOptions = { mode: 'open' as ShadowRootMode };
+  static getTemplateHTML = getTemplateHTML;
+
   #mediaController;
   #isInputTarget;
   #startpoint;
@@ -326,8 +331,15 @@ class MediaChromeRange extends globalThis.HTMLElement {
 
     if (!this.shadowRoot) {
       // Set up the Shadow DOM if not using Declarative Shadow DOM.
-      this.attachShadow({ mode: 'open' });
-      this.shadowRoot.appendChild(template.content.cloneNode(true));
+      this.attachShadow((this.constructor as typeof MediaChromeRange).shadowRootOptions);
+
+      const attrs = namedNodeMapToObject(this.attributes);
+      const html = (this.constructor as typeof MediaChromeRange).getTemplateHTML(attrs);
+      // From MDN: setHTMLUnsafe should be used instead of ShadowRoot.innerHTML 
+      // when a string of HTML may contain declarative shadow roots.
+      this.shadowRoot.setHTMLUnsafe ?
+        this.shadowRoot.setHTMLUnsafe(html) :
+        this.shadowRoot.innerHTML = html;
     }
 
     this.container = this.shadowRoot.querySelector('#container');

examples/nextjs-with-typescript/next-env.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
+/// <reference types="next/navigation-types/compat/navigation" />
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.

examples/nextjs-with-typescript/src/app/react-wrappers/player.tsx
@@ -15,7 +15,7 @@ import {
   MediaAirplayButton,
   MediaPipButton,
   MediaFullscreenButton,
-  MediaPosterImage,
+  MediaErrorDialog,
 } from 'media-chrome/react';
 import {
   MediaPlaybackRateMenu,
@@ -26,6 +26,10 @@ import { useState } from 'react';
 
 const chromeStyles = {
   '--media-primary-color': 'white',
+  display: 'block',
+  width: '100%',
+  aspectRatio: 2.4,
+  maxWidth: 960,
 };
 
 const toggleBool = (prev: boolean|undefined) => !prev;
@@ -51,9 +55,10 @@ export const Player = () => {
         </span>
       </div>
       <br />
-      {mounted && (<MediaController hotkeys={"noarrowleft noarrowright"} style={chromeStyles as any} gesturesDisabled defaultSubtitles noDefaultStore={noDefaultStore}>
+      {mounted && (<MediaController hotkeys={"noarrowleft noarrowright"} style={chromeStyles as any} defaultSubtitles noDefaultStore={noDefaultStore}>
         <video
-          suppressHydrationWarning
+          suppressHydrationWarning={true}
+          style={{ width: '100%', aspectRatio: 2.4 }}
           slot="media"
           src="https://stream.mux.com/A3VXy02VoUinw01pwyomEO3bHnG4P32xzV7u1j1FSzjNg/high.mp4"
           preload="auto"
@@ -73,25 +78,31 @@ export const Player = () => {
             src="./vtt/en-cc.vtt"
           />
         </video>
-        <MediaPosterImage
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
           slot="poster"
-          src="https://image.mux.com/A3VXy02VoUinw01pwyomEO3bHnG4P32xzV7u1j1FSzjNg/thumbnail.jpg"
-          placeholderSrc="data:image/jpeg;base64,/9j/2wBDABQODxIPDRQSEBIXFRQYHjIhHhwcHj0sLiQySUBMS0dARkVQWnNiUFVtVkVGZIhlbXd7gYKBTmCNl4x9lnN+gXz/2wBDARUXFx4aHjshITt8U0ZTfHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHz/wAARCAAUADADASIAAhEBAxEB/8QAGAAAAwEBAAAAAAAAAAAAAAAAAAECBAP/xAAdEAEBAAEEAwAAAAAAAAAAAAAAARECAxITFCFR/8QAGQEAAwADAAAAAAAAAAAAAAAAAAEDAgQF/8QAGBEBAQEBAQAAAAAAAAAAAAAAAAETERL/2gAMAwEAAhEDEQA/ANeC4ldyI1b2EtIzzrrIqYZLvl5FGkGdbfQzGPvo76WsPxXLlfqbaA5va2iVJADgPELACsD/2Q=="
-        ></MediaPosterImage>
+          src="https://image.mux.com/A3VXy02VoUinw01pwyomEO3bHnG4P32xzV7u1j1FSzjNg/thumbnail.webp"
+          style={{
+            width: '100%',
+            height: '100%',
+            background: `center/cover no-repeat url('data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%"><filter id="b" color-interpolation-filters="sRGB"><feGaussianBlur stdDeviation="20"/><feComponentTransfer><feFuncA type="discrete" tableValues="1 1"/></feComponentTransfer></filter><g filter="url(%23b)"><image width="100%" height="100%" preserveAspectRatio="xMidYMid slice" href="data:image/webp;base64,UklGRmYAAABXRUJQVlA4IFoAAABwAgCdASoQAAcAAQAcJbACdLoBJgALN3YuJFiQAP6YWQV+kfwz/U15OyfDLJfktLsUBsNc6MmJGK3NclPSL3/5Cvv4BFA+Uq8P/rs/69v1VTpZxcP6J8wAAAA="/></g></svg>')`,
+          }}
+          alt="woman in misery kneeling down in desert looking up at the sky"
+        />
+        <MediaErrorDialog role="dialog" slot="dialog"></MediaErrorDialog>
         <MediaLoadingIndicator
-          suppressHydrationWarning
           noAutohide
           slot="centered-chrome"
           style={{ '--media-loading-indicator-icon-height': '200px' } as any}
         ></MediaLoadingIndicator>
-        <MediaPlaybackRateMenu hidden anchor="auto" rates={[.5, 1, 2]} />
+        <MediaPlaybackRateMenu role="menu" hidden anchor="auto" rates={[.5, 1, 2]} />
         <MediaControlBar>
-          <MediaPlayButton></MediaPlayButton>
+          <MediaPlayButton mediaPaused={true}></MediaPlayButton>
           <MediaSeekBackwardButton seekOffset={10}></MediaSeekBackwardButton>
           <MediaSeekForwardButton seekOffset={10}></MediaSeekForwardButton>
           <MediaTimeRange></MediaTimeRange>
-          <MediaTimeDisplay showDuration></MediaTimeDisplay>
-          <MediaMuteButton></MediaMuteButton>
+          <MediaTimeDisplay showDuration mediaDuration={134}></MediaTimeDisplay>
+          <MediaMuteButton mediaVolumeLevel="off"></MediaMuteButton>
           <MediaVolumeRange></MediaVolumeRange>
           <MediaPlaybackRateButton rates={[.5, 1, 2]}></MediaPlaybackRateButton>
           <MediaPlaybackRateMenuButton></MediaPlaybackRateMenuButton>

scripts/react/build.js
@@ -26,8 +26,16 @@ const toPascalCase = (kebabText) => {
 const toImportsStr = ({ importPath }) => {
   return `import React from "react";
 import { createComponent } from 'ce-la-react';
-import * as Modules from "${importPath}"
-`;
+import * as Modules from "${importPath}";
+
+function toAttributeValue(propValue) {
+  if (typeof propValue === 'boolean') return propValue ? '' : undefined;
+  if (typeof propValue === 'function') return undefined;
+  const isPrimitive = (v) => typeof v === 'string' || typeof v === 'number' || typeof v === 'boolean';
+  if (Array.isArray(propValue) && propValue.every(isPrimitive)) return propValue.join(' ');
+  if (typeof propValue === 'object' && propValue !== null) return undefined;
+  return propValue;
+}`;
 };
 
 const toReactComponentStr = (config) => {
@@ -38,14 +46,10 @@ export const ${ReactComponentName} = createComponent({
   tagName: "${elementName}",
   elementClass: Modules.${ReactComponentName},
   react: React,
-  toAttributeValue: (propValue) => {
-    if (typeof propValue === 'boolean') return propValue ? '' : undefined;
-    if (typeof propValue === 'function') return undefined;
-    const isPrimitive = (v) => typeof v === 'string' || typeof v === 'number' || typeof v === 'boolean';
-    if (Array.isArray(propValue) && propValue.every(isPrimitive)) return propValue.join(' ');
-    if (typeof propValue === 'object' && propValue !== null) return undefined;
-    return propValue;
-  }
+  toAttributeValue: toAttributeValue,
+  defaultProps: {
+    suppressHydrationWarning: true,
+  },
 });`;
 };
 

src/js/media-airplay-button.ts
@@ -1,5 +1,5 @@
 import { MediaChromeButton } from './media-chrome-button.js';
-import { globalThis, document } from './utils/server-safe-globals.js';
+import { globalThis } from './utils/server-safe-globals.js';
 import { MediaUIEvents, MediaUIAttributes } from './constants.js';
 import { t } from './utils/i18n.js';
 import {
@@ -14,40 +14,43 @@ const airplayIcon = `<svg aria-hidden="true" viewBox="0 0 26 24">
 </svg>
 `;
 
-const slotTemplate: HTMLTemplateElement = document.createElement('template');
-slotTemplate.innerHTML = /*html*/ `
-  <style>
-    :host([${
-      MediaUIAttributes.MEDIA_IS_AIRPLAYING
-    }]) slot[name=icon] slot:not([name=exit]) {
-      display: none !important;
-    }
+function getSlotTemplateHTML(_attrs: Record<string, string>) {
+  return /*html*/ `
+    <style>
+      :host([${
+        MediaUIAttributes.MEDIA_IS_AIRPLAYING
+      }]) slot[name=icon] slot:not([name=exit]) {
+        display: none !important;
+      }
 
-    ${/* Double negative, but safer if display doesn't equal 'block' */ ''}
-    :host(:not([${
-      MediaUIAttributes.MEDIA_IS_AIRPLAYING
-    }])) slot[name=icon] slot:not([name=enter]) {
-      display: none !important;
-    }
+      ${/* Double negative, but safer if display doesn't equal 'block' */ ''}
+      :host(:not([${
+        MediaUIAttributes.MEDIA_IS_AIRPLAYING
+      }])) slot[name=icon] slot:not([name=enter]) {
+        display: none !important;
+      }
 
-    :host([${MediaUIAttributes.MEDIA_IS_AIRPLAYING}]) slot[name=tooltip-enter],
-    :host(:not([${
-      MediaUIAttributes.MEDIA_IS_AIRPLAYING
-    }])) slot[name=tooltip-exit] {
-      display: none;
-    }
-  </style>
+      :host([${MediaUIAttributes.MEDIA_IS_AIRPLAYING}]) slot[name=tooltip-enter],
+      :host(:not([${
+        MediaUIAttributes.MEDIA_IS_AIRPLAYING
+      }])) slot[name=tooltip-exit] {
+        display: none;
+      }
+    </style>
 
-  <slot name="icon">
-    <slot name="enter">${airplayIcon}</slot>
-    <slot name="exit">${airplayIcon}</slot>
-  </slot>
-`;
+    <slot name="icon">
+      <slot name="enter">${airplayIcon}</slot>
+      <slot name="exit">${airplayIcon}</slot>
+    </slot>
+  `;
+}
 
-const tooltipContent = /*html*/ `
-  <slot name="tooltip-enter">${t('start airplay')}</slot>
-  <slot name="tooltip-exit">${t('stop airplay')}</slot>
-`;
+function getTooltipContentHTML() {
+  return /*html*/ `
+    <slot name="tooltip-enter">${t('start airplay')}</slot>
+    <slot name="tooltip-exit">${t('stop airplay')}</slot>
+  `;
+}
 
 const updateAriaLabel = (el: MediaAirplayButton): void => {
   const label = el.mediaIsAirplaying ? t('stop airplay') : t('start airplay');
@@ -67,16 +70,15 @@ const updateAriaLabel = (el: MediaAirplayButton): void => {
  * @event {CustomEvent} mediaairplayrequest
  */
 class MediaAirplayButton extends MediaChromeButton {
+  static getSlotTemplateHTML = getSlotTemplateHTML;
+  static getTooltipContentHTML = getTooltipContentHTML;
+
   static get observedAttributes(): string[] {
     return [
       ...super.observedAttributes,
       MediaUIAttributes.MEDIA_IS_AIRPLAYING,
       MediaUIAttributes.MEDIA_AIRPLAY_UNAVAILABLE,
     ];
-  }
-
-  constructor(options: { slotTemplate?: HTMLTemplateElement } = {}) {
-    super({ slotTemplate, tooltipContent, ...options });
   }
 
   connectedCallback(): void {

commit 11d1e9378f917a0a6f860db7ccf527a0e7c47714

fix: preview alignment bug due to regression introduced in 535ff62

src/js/media-chrome-range.ts
@@ -243,9 +243,15 @@ function getTemplateHTML(_attrs: Record<string, string>) {
         <svg id="segments"><clipPath id="segments-clipping"></clipPath></svg>
       </div>
       <input id="range" type="range" min="0" max="1" step="any" value="0">
+
+      ${this.getContainerTemplateHTML(_attrs)}
     </div>
     <div id="rightgap"></div>
   `;
+}
+
+function getContainerTemplateHTML(_attrs: Record<string, string>) {
+  return '';
 }
 
 /**
@@ -313,6 +319,7 @@ function getTemplateHTML(_attrs: Record<string, string>) {
 class MediaChromeRange extends globalThis.HTMLElement {
   static shadowRootOptions = { mode: 'open' as ShadowRootMode };
   static getTemplateHTML = getTemplateHTML;
+  static getContainerTemplateHTML = getContainerTemplateHTML;
 
   #mediaController;
   #isInputTarget;

src/js/media-time-range.ts
@@ -40,9 +40,8 @@ const updateAriaValueText = (el: any): void => {
   range.setAttribute('aria-valuetext', fullPhrase);
 };
 
-function getTemplateHTML(_attrs: Record<string, string>) {
+function getContainerTemplateHTML(_attrs: Record<string, string>) {
   return /*html*/ `
-    ${MediaChromeRange.getTemplateHTML(_attrs)}
     <style>
       :host {
         --media-box-border-radius: 4px;
@@ -408,7 +407,7 @@ const calcTimeFromRangeValue = (
  */
 class MediaTimeRange extends MediaChromeRange {
   static shadowRootOptions = { mode: 'open' as ShadowRootMode };
-  static getTemplateHTML = getTemplateHTML;
+  static getContainerTemplateHTML = getContainerTemplateHTML;
 
   static get observedAttributes(): string[] {
     return [